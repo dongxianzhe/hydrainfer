a415b68efb027a78f89903d43862f19b80527b26
Xianzhe Dong <dongxianzhe2019@gmail.com>
2024-09-29 02:53:46 -0400

    add forward for each gpt2 module

--- dxz/model/gpt2.py
+++ dxz/model/gpt2.py
@@ -1,31 +1,42 @@
+from typing import List
+
 import math
 import torch
 from torch import nn
 from torch import Tensor
+from transformers import GPT2Config
 
 class NewGELUActivation(nn.Module):
-    """
-    Implementation of the GELU activation function currently in Google BERT repo (identical to OpenAI GPT). Also see
-    the Gaussian Error Linear Units paper: https://arxiv.org/abs/1606.08415
-    """
     def forward(self, input: Tensor) -> Tensor:
         return 0.5 * input * (1.0 + torch.tanh(math.sqrt(2.0 / math.pi) * (input + 0.044715 * torch.pow(input, 3.0))))
 
 class GPT2Attention(nn.Module):
-    def __init__(self, config, layer_idx=None):
-        self.c_attn = nn.Conv1D(3 * self.embed_dim, self.embed_dim)
-        self.c_proj = nn.Conv1D(self.embed_dim, self.embed_dim)
+    def __init__(self, config: GPT2Config, layer_idx=None):
+        self.c_attn = nn.linear(3 * self.embed_dim, self.embed_dim, bias=True)
+        self.c_proj = nn.linear(self.embed_dim, self.embed_dim, bias=True)
         self.atten = nn.MultiheadAttention()
+    
+    def forward(self, hidden_states: Tensor, kv_cache: Tensor) -> Tensor:
+        qkv = self.c_attn(hidden_states)
+        q, k, v = qkv.chunk(chunks=3, dim=-1)
+        attn_output = self.atten(q, k, v, kv_cache)
+        attn_output = self.c_proj(attn_output)
+        return attn_output
 
 class GPT2MLP(nn.Module):
-    def __init__(self, intermediate_size, config):
-        embed_dim = config.hidden_size
-        self.c_fc = nn.Conv1D(intermediate_size, embed_dim)
-        self.c_proj = nn.Conv1D(embed_dim, intermediate_size)
+    def __init__(self, intermediate_size: int, config: GPT2Config):
+        self.c_fc = nn.Linear(intermediate_size, config.hidden_size, bias=True)
+        self.c_proj = nn.Linear(config.hidden_size, intermediate_size, bias=True)
         self.act = NewGELUActivation()
+    
+    def forward(self, hidden_states: Tensor) -> Tensor:
+        hidden_states = self.c_fc(hidden_states)
+        hidden_states = self.act(hidden_states)
+        hidden_states = self.c_proj(hidden_states)
+        return hidden_states
 
 class GPT2Block(nn.Module):
-    def __init__(self, config, layer_idx):
+    def __init__(self, config: GPT2Config, layer_idx):
         super(GPT2Block, self).__init__()
         hidden_size = config.hidden_size
         inner_dim = 4 * hidden_size
@@ -35,8 +46,20 @@ class GPT2Block(nn.Module):
         self.ln_2 = nn.LayerNorm(hidden_size, eps=config.layer_norm_epsilon)
         self.mlp = GPT2MLP(inner_dim, config)
 
+    def forward(self, hidden_states: Tensor, kv_cache: Tensor) -> Tensor:
+        residual = hidden_states
+        hidden_states = self.ln_1(hidden_states)
+        attn_output = self.attn(hidden_states, kv_cache)
+        hidden_states = attn_output + residual
+
+        residual = hidden_states
+        hidden_states = self.ln_2(hidden_states)
+        feed_forward_hidden_states = self.mlp(hidden_states)
+        hidden_states = residual + feed_forward_hidden_states
+        return hidden_states
+
 class GPT2Model(nn.Module):
-    def __init__(self, config):
+    def __init__(self, config: GPT2Config):
         super(GPT2Model, self).__init__()
         self.embed_dim = config.hidden_size
 
@@ -44,3 +67,12 @@ class GPT2Model(nn.Module):
         self.wpe = nn.Embedding(config.max_position_embeddings, self.embed_dim)
         self.h = nn.ModuleList([GPT2Block(config, layer_idx=i) for i in range(config.num_hidden_layers)])
         self.ln_f = nn.LayerNorm(self.embed_dim, eps=config.layer_norm_epsilon)
+    
+    def forward(self, input_ids: Tensor, position_ids: Tensor, kv_caches: List[Tensor]):
+        input_embeds = self.wte(input_ids)
+        position_embeds = self.wpe(position_ids)
+        hidden_states = input_embeds + position_embeds
+        for i, layer in enumerate(self.h):
+            hidden_states = layer(hidden_states, kv_caches[i])
+        hidden_states = self.ln_f(hidden_states)
+        return hidden_states
