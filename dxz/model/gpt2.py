717e4100f9cadfecfa9da37483f1fd5ffed90f45
Xianzhe Dong <dongxianzhe2019@gmail.com>
2024-09-29 04:21:24 -0400

    test gpt2 model output

--- dxz/model/gpt2.py
+++ dxz/model/gpt2.py
@@ -11,7 +11,7 @@ class NewGELUActivation(nn.Module):
         return 0.5 * input * (1.0 + torch.tanh(math.sqrt(2.0 / math.pi) * (input + 0.044715 * torch.pow(input, 3.0))))
 
 class Attention(nn.Module):
-    def forward(self, query: Tensor, key: Tensor, value: Tensor, kv_cache: Optional[Tensor]) -> Tensor:
+    def forward(self, query: Tensor, key: Tensor, value: Tensor) -> Tensor:
         # todo
         return query
 
@@ -22,18 +22,20 @@ class GPT2Attention(nn.Module):
         self.c_proj = nn.Linear(config.hidden_size, config.hidden_size, bias=True)
         self.atten = Attention()
     
-    def forward(self, hidden_states: Tensor, kv_cache: Tensor) -> Tensor:
+    def forward(self, hidden_states: Tensor) -> Tensor:
         qkv = self.c_attn(hidden_states)
         q, k, v = qkv.chunk(chunks=3, dim=-1)
-        attn_output = self.atten(q, k, v, kv_cache)
+        attn_output = self.atten(q, k, v)
         attn_output = self.c_proj(attn_output)
         return attn_output
 
 class GPT2MLP(nn.Module):
-    def __init__(self, intermediate_size: int, config: GPT2Config):
+    def __init__(self, config: GPT2Config):
         super(GPT2MLP, self).__init__()
-        self.c_fc = nn.Linear(intermediate_size, config.hidden_size, bias=True)
-        self.c_proj = nn.Linear(config.hidden_size, intermediate_size, bias=True)
+        assert config.n_inner is None, "only support intermediate = 4 * hidden_size now"
+        intermediate_size = config.hidden_size * 4
+        self.c_fc = nn.Linear(config.hidden_size, intermediate_size, bias=True)
+        self.c_proj = nn.Linear(intermediate_size, config.hidden_size, bias=True)
         self.act = NewGELUActivation()
     
     def forward(self, hidden_states: Tensor) -> Tensor:
@@ -45,18 +47,16 @@ class GPT2MLP(nn.Module):
 class GPT2Block(nn.Module):
     def __init__(self, config: GPT2Config):
         super(GPT2Block, self).__init__()
-        hidden_size = config.hidden_size
-        inner_dim = 4 * hidden_size
 
-        self.ln_1 = nn.LayerNorm(hidden_size, eps=config.layer_norm_epsilon)
+        self.ln_1 = nn.LayerNorm(config.hidden_size, eps=config.layer_norm_epsilon)
         self.attn = GPT2Attention(config=config)
-        self.ln_2 = nn.LayerNorm(hidden_size, eps=config.layer_norm_epsilon)
-        self.mlp = GPT2MLP(inner_dim, config)
+        self.ln_2 = nn.LayerNorm(config.hidden_size, eps=config.layer_norm_epsilon)
+        self.mlp = GPT2MLP(config)
 
-    def forward(self, hidden_states: Tensor, kv_cache: Tensor) -> Tensor:
+    def forward(self, hidden_states: Tensor) -> Tensor:
         residual = hidden_states
         hidden_states = self.ln_1(hidden_states)
-        attn_output = self.attn(hidden_states, kv_cache)
+        attn_output = self.attn(hidden_states)
         hidden_states = attn_output + residual
 
         residual = hidden_states
@@ -74,12 +74,12 @@ class GPT2Model(nn.Module):
         self.h = nn.ModuleList([GPT2Block(config) for _ in range(config.num_hidden_layers)])
         self.ln_f = nn.LayerNorm(self.embed_dim, eps=config.layer_norm_epsilon)
     
-    def forward(self, input_ids: Tensor, position_ids: Tensor, kv_caches: List[Tensor]):
+    def forward(self, input_ids: Tensor, position_ids: Tensor):
         input_embeds = self.wte(input_ids)
         position_embeds = self.wpe(position_ids)
         hidden_states = input_embeds + position_embeds
         for i, layer in enumerate(self.h):
-            hidden_states = layer(hidden_states, kv_caches[i])
+            hidden_states = layer(hidden_states)
         hidden_states = self.ln_f(hidden_states)
         return hidden_states
 
@@ -130,5 +130,20 @@ def test_model_name():
             print('------------- dif --------------')
     print('pass')
 
+def test_model_output():
+    config = GPT2Config()
+    model = GPT2Model(config)
+    from transformers import GPT2Model as GPT2ModelRef
+    model_ref = GPT2ModelRef.from_pretrained('gpt2')
+
+    input_ids = torch.arange(10)
+    position_ids = torch.arange(10)
+
+    output = model(input_ids=input_ids, position_ids=position_ids)
+    output_ref = model_ref(input_ids=input_ids, position_ids=position_ids)
+
+    assert torch.allclose(output, output_ref['last_hidden_state'], rtol=1e-3, atol=1e-3)
+    print('pass')
+
 if __name__ == '__main__':
-    test_model_name()
+    test_model_output()
