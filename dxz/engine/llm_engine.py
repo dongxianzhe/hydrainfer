ff0bfd3ba85f000aa58c59a9fdb4de8da28534eb
Xianzhe Dong <dongxianzhe2019@gmail.com>
2024-10-05 20:56:20 -0400

    add llm engine greedy decode

--- dxz/engine/llm_engine.py
+++ dxz/engine/llm_engine.py
@@ -12,22 +12,18 @@ class LLMEngine:
     def generate(self, prompts: list[str]) -> list[str]:
         results = []
         for prompt in prompts:
-            encoded_input = self.tokenizer.encode(prompt, return_tensors='pt')[0] # token list
-            print(f'encoded_input.shape: {encoded_input.shape}')
-            position_ids = torch.arange(encoded_input.shape[-1])
-            logits = self.model(input_ids=encoded_input, position_ids=position_ids)['logits']
-            next_token_logits = logits[-1, :]
-            next_token_id = torch.argmax(next_token_logits, dim=-1, keepdim=True)
+            encoded_input = self.tokenizer.encode(prompt, return_tensors='pt')[0] # token list # (n_tokens, )
+            for _ in range(50):
+                position_ids = torch.arange(encoded_input.shape[-1]) # (n_tokens, )
+                logits = self.model(input_ids=encoded_input, position_ids=position_ids)['logits'] # (n_tokens, vocab_size)
+                next_token_logits = logits[-1, :] # (vocab_size, )
+                next_token_id = torch.argmax(next_token_logits, dim=-1, keepdim=True) # (1, )
 
-            print(f'next_token_id: {next_token_id}')
-            if(next_token_id == self.tokenizer.eos_token_id):
-                print(f'next_token_id: {next_token_id} is eos token')
-                break
-            print(f'logits.shape       : {logits.shape}')
+                if(next_token_id == self.tokenizer.eos_token_id):
+                    break
 
-            encoded_input = torch.cat((encoded_input, next_token_id), dim=0)
+                encoded_input = torch.cat((encoded_input, next_token_id), dim=0) # (n_tokens + 1, )
 
-            # todo
             decoded_output = self.tokenizer.decode(encoded_input)
             results.append(decoded_output)
         
@@ -35,6 +31,6 @@ class LLMEngine:
 
 if __name__ == '__main__':
     llm_engine = LLMEngine()
-    results = llm_engine.generate(["Hello, I'm a language model,"])
+    results = llm_engine.generate(["Hello"])
     for result in results:
         print(result)
