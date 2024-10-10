80705be0b6f74d9b39591fbf16b30b592de26d86
Xianzhe Dong <dongxianzhe2019@gmail.com>
2024-10-10 03:29:32 -0400

    refactor gpt2 activation function to layer folder

---
+++ dxz/layer/activation.py
@@ -0,0 +1,7 @@
+import torch
+from torch import nn, Tensor
+import math
+
+class NewGELUActivation(nn.Module):
+    def forward(self, input: Tensor) -> Tensor:
+        return 0.5 * input * (1.0 + torch.tanh(math.sqrt(2.0 / math.pi) * (input + 0.044715 * torch.pow(input, 3.0))))
