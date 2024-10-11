b91ec731d0f8a0582989b4af6974989035d97df0
Xianzhe Dong <dongxianzhe2019@gmail.com>
2024-10-11 08:14:14 -0400

    add debug utils

--- dxz/utils/debug.py
+++ dxz/utils/debug.py
@@ -1,6 +1,25 @@
-def print_once(message: str):
-    if not hasattr(print_once, 'has_printed'):
-        print_once.has_printed = False  # 初始化属性
-    if not print_once.has_printed:
-        print(message)
-        print_once.has_printed = True  # 更新状态
+import torch
+
+def print_once(scope:str="default", message: str=""):
+    if not hasattr(print_once, 'scope_printed'):
+        print_once.scope_printed: dict[str, bool] = {}
+    if scope not in print_once.scope_printed:
+        print_once.scope_printed[scope]=False
+    if not print_once.scope_printed[scope]:
+        print_once.scope_printed[scope]=True
+        print(f"{scope}: {message}")
+
+def probe(key: str, value: any):
+    if not hasattr(probe, 'data'):
+        probe.data = {}
+    if key not in probe.data:
+        probe.data[key] = value
+    if not torch.allclose(probe.data[key], value, rtol=1e-3, atol=1e-5):
+        print(f'probe {key} not equal !!! ')
+
+
+if __name__ == '__main__':
+    x = torch.zeros(size=(10, 128))
+    y = torch.zeros(size=(10, 128))
+    probe('h', x)
+    probe('h', y)
