38f9afc6edb4766ca5f42e5809d1c7039e7de972
Xianzhe Dong <dongxianzhe2019@gmail.com>
2024-10-11 02:55:57 -0400

    [ut] test rotary embedding layer

--- dxz/layer/rotary_embedding.py
+++ dxz/layer/rotary_embedding.py
@@ -1,8 +1,15 @@
 import torch
 from torch import nn, Tensor
 
+def compute_default_inv_freq(rotary_dim: int, theta: float) -> Tensor:
+    # return [a, b] (rotary_dim / 2, )
+    assert rotary_dim % 2 == 0, "rotary_dim must be even"
+    slice = torch.arange(0, rotary_dim, 2, dtype=torch.float)
+    return 1. / torch.pow(theta, slice / rotary_dim)
+
 class RotaryEmbedding(nn.Module):
     def __init__(self, rotary_dim: int, max_position_embeddings: int, inv_freq: Tensor, interleaved: bool):
+        super().__init__()
         # rotary_dim <= head_dim, rotary rotary_dim elements
         # interleaved = True means adjacent two elements rotary as a pair
         self.rotary_dim              = rotary_dim
@@ -19,23 +26,23 @@ class RotaryEmbedding(nn.Module):
             # freq [a, b] => emd [a, b, a, b]
             emd = torch.cat([freqs, freqs], dim=-1)
         # emd (max_position_embedding, rotary_dim)
-        cos_sin = torch.cat([emd.cos(), emd.sin()], dim=-1) 
+        cos_sin = torch.cat([emd.cos(), emd.sin()], dim=-1)
         # cos_sin (max_position_embedding, rotary_dim * 2)
         # [ca, ca, cb, cb, sa, sa, sb, sb] or [ca, cb, ca, cb, sa, sb, sa, sb]
-        self.cos_sin_cache = self.register_buffer(name='cos_sin_cache', tensor=cos_sin)
+        self.register_buffer(name='cos_sin_cache', tensor=cos_sin)
     
-    def forward(query: Tensor, key: Tensor, position_ids: Tensor) -> tuple[Tensor, Tensor]:
+    def forward(self, query: Tensor, key: Tensor, position_ids: Tensor) -> tuple[Tensor, Tensor]:
         # query (num_tokens, n_qo_heads, head_dim)
         # key   (num_tokens, n_kv_heads, head_dim)
         # position_ids (num_tokens, )
-        query_rotary = query[:, :, : rotary_dim]
-        query_pass   = query[:, :, rotary_dim: ]
-        key_rotary = key[:, :, : rotary_dim]
-        key_pass   = key[:, :, rotary_dim: ]
+        query_rotary = query[:, :, : self.rotary_dim]
+        query_pass   = query[:, :, self.rotary_dim: ]
+        key_rotary = key[:, :, : self.rotary_dim]
+        key_pass   = key[:, :, self.rotary_dim: ]
 
         # cos_sin (num_tokens, rotary_dim * 2)
-        cos_sin = torch.nn.functional.embedding(position_ids, self.cos_sin_cache)
-        cos,sin = cos_sin.chunk(chunks=2, dim=-1) # cos(num_tokens, head_dim) sin(num_tokens, head_dim)
+        cos_sin = torch.nn.functional.embedding(position_ids, self.cos_sin_cache)[:, None, :]
+        cos,sin = cos_sin.chunk(chunks=2, dim=-1) # cos(num_tokens, 1, head_dim) sin(num_tokens, 1, head_dim)
         if self.interleaved:
             # rotary_dim = 4
             # cos [ca, ca, cb, cb]
@@ -60,23 +67,19 @@ class RotaryEmbedding(nn.Module):
             # counterclock
             query_rotary = (query_rotary * cos) + (self.rotate_half(query_rotary) * sin)
             key_rotary = (key_rotary * cos) + (self.rotate_half(key_rotary) * sin)
-            pass
         return (torch.cat([query_rotary, query_pass], dim=-1), torch.cat([key_rotary, key_pass], dim=-1))
         
     def rotate_every_two(self, x: Tensor) -> Tensor:
+        # (n_tokens, n_heads, head_dim)
         # [0, 1, 2, 3] => [-1, 0, -3, 2]
-        x1 = x[::2] # [0, 2]
-        x2 = x[1::2] # [1, 3]
+        x1 = x[:, :, ::2] # [0, 2]
+        x2 = x[:, :, 1::2] # [1, 3]
         x = torch.stack([-x2, x1], dim=-1) # [[-1, 0], [-3, 2]]
         x = x.flatten(start_dim=-2) #  [-1, 0, -3, 2]
         return x
     
-    def rotary_half(self, x: Tensor) -> Tensor:
+    def rotate_half(self, x: Tensor) -> Tensor:
+        # (n_tokens, n_heads, head_dim)
         # [0, 1, 2, 3] => [-2, -3, 0, 1]
         x1, x2 = x.chunk(chunks=2, dim=-1) # [0, 1] [2, 3]
         return torch.cat([-x2, x1], dim=-1) # [-2, -3, 0, 1]
-
-if __name__ == '__main__':
-    x = torch.arange(4)
-    y = rotary_half(x)
-    print(y)
---
+++ tests/layer/test_rotary_embedding.py
@@ -0,0 +1,107 @@
+import torch
+from torch import nn, Tensor
+from dxz.layer.rotary_embedding import RotaryEmbedding, compute_default_inv_freq
+import pytest
+from torch.nn import functional as F
+
+def precompute_freqs_cis(dim, max_position_embeddings, theta):
+    range_tensor = torch.arange(0, dim, 2, dtype=torch.float32)
+    slice_tensor = range_tensor[:dim // 2]
+    freqs = 1.0 / torch.pow(theta, slice_tensor / dim)
+    t = torch.arange(max_position_embeddings, dtype=torch.float32)
+    freqs = torch.outer(t, freqs)
+    return torch.polar(torch.ones_like(freqs), freqs)
+
+# [1, 2, 3, 4, 5, 6] => [1, 3, 5, 2, 4, 6]
+def interleaved_to_half(x):
+    x1 = x[..., ::2]
+    x2 = x[..., 1::2]
+    return torch.cat([x1, x2], dim=-1)
+
+# [1, 3, 5, 2, 4, 6] -> [1, 2, 3, 4, 5, 6]
+def half_to_interleaved(x):
+    chunks = x.chunk(2, dim=-1)
+    return torch.stack([chunks[0], chunks[1]], dim=-1).flatten(start_dim=-2)
+
+def split_tensor_by_last_dim(x):
+    shape = list(x.shape)
+    shape[-1] = -1
+    shape.append(2)
+    return x.reshape(shape)
+
+def apply_rotary_emb(xq, xk, freqs_cis):
+    # Convert to complex numbers
+    xq_complex = torch.view_as_complex(split_tensor_by_last_dim(xq.to(torch.float)))
+    xk_complex = torch.view_as_complex(split_tensor_by_last_dim(xk.to(torch.float)))
+
+    # Reshape for broadcast at n_heads dim
+    freqs_cis = freqs_cis.unsqueeze(1)
+
+    # Apply rotation
+    xq_out = torch.view_as_real(xq_complex * freqs_cis).flatten(2)
+    xk_out = torch.view_as_real(xk_complex * freqs_cis).flatten(2)
+
+    # Cast back to original dtype
+    xq_out = xq_out.type_as(xq)
+    xk_out = xk_out.type_as(xk)
+
+    return xq_out, xk_out
+
+def apply_rotary_emb_ref(query:Tensor, key:Tensor, positions:Tensor, head_dim:int, max_position_embeddings:int, theta:float, interleaved:bool)->tuple[Tensor, Tensor]:
+    freqs_cis = precompute_freqs_cis(head_dim, max_position_embeddings, theta)
+    selected_freqs_cis = F.embedding(positions, freqs_cis)
+
+    if interleaved:
+        return apply_rotary_emb(query, key, selected_freqs_cis) 
+    else:
+        interleaved_query = half_to_interleaved(query) 
+        interleaved_key = half_to_interleaved(key) 
+        query_ref, key_ref = apply_rotary_emb(interleaved_query, interleaved_key, selected_freqs_cis)
+        query_ref = interleaved_to_half(query_ref) 
+        key_ref = interleaved_to_half(key_ref) 
+        return query_ref, key_ref
+
+@pytest.mark.parametrize("device", [torch.device('cpu'), torch.device('cuda:0')])
+@pytest.mark.parametrize("dtype", [torch.float])
+@pytest.mark.parametrize("num_tokens", [1, 2, 8, 16])
+@pytest.mark.parametrize("n_heads", [32])
+@pytest.mark.parametrize("n_kv_heads", [32, 8, 1])
+@pytest.mark.parametrize("head_dim", [128])
+@pytest.mark.parametrize("theta", [100000., 500000.])
+@pytest.mark.parametrize("interleaved", [False, True])
+@pytest.mark.parametrize("max_position_embeddings", [4096, 8192])
+def test_rotary_correctness(device, dtype, num_tokens, n_heads, n_kv_heads, head_dim, theta, interleaved, max_position_embeddings):
+    rotary_embedding = RotaryEmbedding(
+        rotary_dim=head_dim, # rotary all elements
+        max_position_embeddings=max_position_embeddings,
+        inv_freq=compute_default_inv_freq(rotary_dim=head_dim, theta=theta),
+        interleaved=interleaved
+        )
+    rotary_embedding.to(device)
+    query = torch.randn(size=(num_tokens, n_heads, head_dim), device=device)
+    key = torch.randn(size=(num_tokens, n_kv_heads, head_dim), device=device)
+    position_ids = torch.randint(0, max_position_embeddings, size=(num_tokens, ), dtype=torch.int, device=device)
+    query_output, key_output = rotary_embedding(query, key, position_ids)
+
+    # ref
+    query = query.to(torch.device('cpu'))
+    key = key.to(torch.device('cpu'))
+    position_ids = position_ids.to(torch.device('cpu'))
+
+    query_ref, key_ref = apply_rotary_emb_ref(
+        query = query, 
+        key = key, 
+        positions = position_ids, 
+        head_dim = head_dim, 
+        max_position_embeddings = max_position_embeddings, 
+        theta = theta, 
+        interleaved = interleaved
+    )
+
+    query_output = query_output.to(torch.device('cpu'))
+    key_output = key_output.to(torch.device('cpu'))
+    assert torch.allclose(query_ref, query_output, rtol=1e-3, atol=1e-5) 
+    assert torch.allclose(key_ref, key_output, rtol=1e-3, atol=1e-5) 
+
+if __name__ == "__main__":
+    pytest.main([__file__])
