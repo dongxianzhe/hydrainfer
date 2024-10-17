9942b5ab79087d054c945bea5db68566bcf71aa5
Xianzhe Dong <dongxianzhe2019@gmail.com>
2024-10-17 03:16:47 -0700

    add benchmark/dataset and download script

---
+++ benchmark/dataset/download.sh
@@ -0,0 +1,2 @@
+curl -L -o sharegpt_gpt4.jsonl https://huggingface.co/datasets/shibing624/sharegpt_gpt4/resolve/main/sharegpt_gpt4.jsonl
+curl -L -o ShareGPT_V3_unfiltered_cleaned_split.json https://huggingface.co/datasets/learnanything/sharegpt_v3_unfiltered_cleaned_split/resolve/main/ShareGPT_V3_unfiltered_cleaned_split.json
