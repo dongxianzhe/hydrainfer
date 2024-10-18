5e33cd164dbae9b667337dca795270142beba212
Xianzhe Dong <dongxianzhe2019@gmail.com>
2024-10-17 23:00:07 -0700

    test request func

--- benchmark/backend_request_func.py
+++ benchmark/backend_request_func.py
@@ -4,17 +4,13 @@ import aiohttp
 from tqdm import tqdm
 import time
 import json
+import asyncio
+import traceback
+import sys
 
 @dataclass
 class RequestFuncInput:
     prompt: str
-    api_url: str
-    prompt_len: int
-    output_len: int
-    model: str
-    best_of: int = 1
-    use_beam_search: bool = False
-
 
 @dataclass
 class RequestFuncOutput:
@@ -23,7 +19,6 @@ class RequestFuncOutput:
     latency: float = 0
     ttft: float = 0  # Time to first token
     itl: List[float] = field(default_factory=list)  # List of inter-token latencies
-    prompt_len: int = 0
     error: str = ""
 
 AIOHTTP_TIMEOUT = aiohttp.ClientTimeout(total=6 * 60 * 60)
@@ -32,48 +27,62 @@ async def request_func(
     request_func_input: RequestFuncInput,
     pbar: Optional[tqdm] = None,
 ) -> RequestFuncOutput:
-    api_url = request_func_input.api_url
     async with aiohttp.ClientSession(timeout=AIOHTTP_TIMEOUT) as session:
+        headers = {
+            'Content-Type': 'application/json'
+        }
         payload = {
-            "prompt": request_func_input.prompt,
-            "temperature": 0.0,
-            "best_of": request_func_input.best_of,
-            "max_tokens": request_func_input.output_len,
-            "top_k": 50,
+            "model": "gpt2",
+            "prompt": request_func_input.prompt, 
+            "max_tokens": 50,
+            "temperature": 0, 
             "stream": True,
-            "ignore_eos": True,
         }
         output = RequestFuncOutput()
-        output.prompt_len = request_func_input.prompt_len
-        generated_text = ""
-        ttft = 0
-        st = time.perf_counter()
-        most_recent_timestamp = st
+        most_recent_timestamp = start_timestamp = time.perf_counter()
         try:
-            async with session.post(url=api_url, json=payload) as response:
+            async with session.post(url='http://127.0.0.1:8888/v1/completions', json=payload, headers=headers) as response:
                 if response.status == 200:
-                    async for chunk, _ in response.content.iter_chunks:
-                        chunk = chunk.strip()
-                        if not chunk:
-                            continue
-                        chunk = chunk[:-1]
-                        data = json.loads(chunk.decode("utf-8"))
-                        if data['text']:
-                            timestamp = time.perf_counter()
-                            if ttft == 0:
-                                ttft = time.perf_counter - st
-                            else:
-                                output.itl.append(timestamp - most_recent_timestamp)
-                            most_recent_timestamp = timestamp
-                            generated_text = data['text']
-                    latency = time.perf_counter - st
-                    output.generated_text = generated_text
+                    async for chunk, _ in response.content.iter_chunks():
+                        # eg. b'data: {"id":"cmpl-5b2c049fe2664f2ca7f12915b5e4a8f9","object":"text_completion","created":1729229567,"model":"gpt2","choices":[{"index":0,"text":",","logprobs":null,"finish_reason":null,"stop_reason":null}],"usage":null}\n\n'
+                        # eg. b'data: [DONE]\n\n'
+                        timestamp = time.perf_counter()
+                        if output.ttft == 0:
+                            output.ttft = timestamp - start_timestamp
+                        else:
+                            output.itl.append(timestamp - most_recent_timestamp)
+                        most_recent_timestamp = timestamp
+
+                        chunk = chunk.strip() # remove \n\n
+                        chunk = chunk[6:] # remove data: 
+                        if not chunk or chunk == b'[DONE]':
+                            pass
+                        else:
+                            data = json.loads(chunk.decode("utf-8")) # convert to json
+                            output.generated_text += data['choices'][0]['text']
+                    latency = time.perf_counter() - start_timestamp
                     output.success = True
                     output.latency = latency
         except:
             print(chunk)
             output.success = False
+            print(traceback.format_exc())
+            exc_info = sys.exc_info()
+            output.error = "".join(traceback.format_exception(*exc_info))
         
     if pbar:
         pbar.update(1)
     return output
+
+# async def main():
+#     input = RequestFuncInput(
+#         prompt = "Hello",
+#     )
+
+#     output = await request_func(input)
+#     print(input.prompt + output.generated_text)
+#     print(f'------------------------------------------------------------------------------------------')
+#     print(output)
+
+# if __name__ == '__main__':
+#     asyncio.run(main())
