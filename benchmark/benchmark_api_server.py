18e6764cd4626bb2708b36f1e0cac5740aea8416
Xianzhe Dong <dongxianzhe2019@gmail.com>
2024-10-17 22:16:09 -0700

    refactor request func

--- benchmark/benchmark_api_server.py
+++ benchmark/benchmark_api_server.py
@@ -9,81 +9,8 @@ from typing import List, Tuple, AsyncGenerator, Optional
 import argparse
 import numpy as np
 import asyncio
-
-@dataclass
-class RequestFuncInput:
-    prompt: str
-    api_url: str
-    prompt_len: int
-    output_len: int
-    model: str
-    best_of: int = 1
-    use_beam_search: bool = False
-
-
-@dataclass
-class RequestFuncOutput:
-    generated_text: str = ""
-    success: bool = False
-    latency: float = 0
-    ttft: float = 0  # Time to first token
-    itl: List[float] = field(default_factory=list)  # List of inter-token latencies
-    prompt_len: int = 0
-    error: str = ""
-
-AIOHTTP_TIMEOUT = aiohttp.ClientTimeout(total=6 * 60 * 60)
-
-async def request_func(
-    request_func_input: RequestFuncInput,
-    pbar: Optional[tqdm] = None,
-) -> RequestFuncOutput:
-    api_url = request_func_input.api_url
-    async with aiohttp.ClientSession(timeout=AIOHTTP_TIMEOUT) as session:
-        payload = {
-            "prompt": request_func_input.prompt,
-            "temperature": 0.0,
-            "best_of": request_func_input.best_of,
-            "max_tokens": request_func_input.output_len,
-            "top_k": 50,
-            "stream": True,
-            "ignore_eos": True,
-        }
-        output = RequestFuncOutput()
-        output.prompt_len = request_func_input.prompt_len
-        generated_text = ""
-        ttft = 0
-        st = time.perf_counter()
-        most_recent_timestamp = st
-        try:
-            async with session.post(url=api_url, json=payload) as response:
-                if response.status == 200:
-                    async for chunk, _ in response.content.iter_chunks:
-                        chunk = chunk.strip()
-                        if not chunk:
-                            continue
-                        chunk = chunk[:-1]
-                        data = json.loads(chunk.decode("utf-8"))
-                        if data['text']:
-                            timestamp = time.perf_counter()
-                            if ttft == 0:
-                                ttft = time.perf_counter - st
-                            else:
-                                output.itl.append(timestamp - most_recent_timestamp)
-                            most_recent_timestamp = timestamp
-                            generated_text = data['text']
-                    latency = time.perf_counter - st
-                    output.generated_text = generated_text
-                    output.success = True
-                    output.latency = latency
-        except:
-            print(chunk)
-            output.success = False
-        
-    if pbar:
-        pbar.update(1)
-    return output
-
-
+from backend_request_func import RequestFuncInput, RequestFuncOutput, request_func
+from sample_dataset import sample_sharegpt_requests
 
 async def get_request(
     input_requests: List[Tuple[str, int, int]],
