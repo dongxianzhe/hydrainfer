5635f81d8dd34ae6618558ac4eabc03b4633f1d8
Xianzhe Dong <dongxianzhe2019@gmail.com>
2024-10-28 21:44:17 -0700

    [refactor][ut] refactor block allocator and add it's unitest.

--- dxz/memory/block_allocator.py
+++ dxz/memory/block_allocator.py
@@ -5,14 +5,15 @@ class BlockAllocator:
         self.free_blocks: list[int] = [block_id for block_id in reversed(range(total_blocks))]
 
     def allocate(self, n_blocks: int) -> list[int]:
-        assert n_blocks <= len(self.free_blocks), "not enough block to allocate"
-        blocks: list[int] = []
-        for _ in range(n_blocks):
-            block_id = self.free_blocks.pop()
-            blocks.append(block_id)
+        if n_blocks == 0:
+            return []
+        if n_blocks > len(self.free_blocks):
+            # not enough block to allocate
+            return []
+
+        blocks, self.free_blocks = self.free_blocks[-n_blocks:], self.free_blocks[:-n_blocks]
         return blocks
     
     def free(self, blocks: list[int]) -> bool:
-        for block_id in blocks:
-            assert block_id >= 0 and block_id < self.total_blocks, f"invalid block id {block_id}"
-            self.free_blocks.append(block_id)
+        self.free_blocks += blocks
+        assert len(self.free_blocks) <= self.total_blocks
---
+++ tests/memory/test_block_allocator.py
@@ -0,0 +1,42 @@
+import pytest
+from dxz.memory.block_allocator import BlockAllocator
+import random
+
+def test_allocate():
+    total_blocks = 100
+    allocator = BlockAllocator(total_blocks)
+    # test random allocate
+    free_blocks_ref: list[int] = [block_id for block_id in reversed(range(total_blocks))]
+    for i in range(10):
+        n = random.randint(1, 10)
+        blocks = allocator.allocate(n)
+
+        blocks_ref = []
+        for _ in range(n):
+            blocks_ref.append(free_blocks_ref.pop())
+        blocks_ref.reverse()
+
+        assert blocks == blocks_ref 
+
+    # test allocate zero
+    blocks = allocator.allocate(0)
+
+def test_out_of_memory():
+    total_blocks = 10
+    allocator = BlockAllocator(total_blocks)
+    print(allocator.allocate(11))
+    assert allocator.allocate(11) == []
+    assert allocator.allocate(2) == [1, 0]
+    assert allocator.allocate(9) == []
+    assert allocator.allocate(8) == [9, 8, 7, 6, 5, 4, 3, 2]
+    assert allocator.allocate(1) == []
+
+def test_free():
+    total_blocks = 10
+    allocator = BlockAllocator(total_blocks)
+    assert allocator.allocate(3) == [2, 1, 0]
+    allocator.free([1, 0])
+    assert allocator.allocate(3) == [3, 1, 0]
+
+if __name__ == '__main__':
+    pytest.main([__file__])
