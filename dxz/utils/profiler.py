61bfc2b1e84797143af136e7f36f75bdefd51d1f
Xianzhe Dong <dongxianzhe2019@gmail.com>
2024-11-11 21:48:34 -0800

    [feat] add profiler total time print as process epilogue.

--- dxz/utils/profiler.py
+++ dxz/utils/profiler.py
@@ -1,6 +1,14 @@
 import time
 import torch
 from contextlib import contextmanager
+import atexit
+
+context_total_times:dict[str, float] = {}
+def print_profile_result():
+    print('context_name   total_time')
+    for context_name, total_time in context_total_times.items():
+        print(f'{context_name:12}   {total_time:12} s')
+
 
 @contextmanager
 def profile(context_name: str, profile_time:bool=True, profile_nvtx_range:bool=True):
@@ -17,4 +25,7 @@ def profile(context_name: str, profile_time:bool=True, profile_nvtx_range:bool=T
         torch.cuda.nvtx.range_pop()
 
     if profile_time:
+        context_total_times[context_name] = context_total_times.get(context_name, 0) + end_time - start_time
         print(f'{context_name} took {end_time - start_time:.2}s')
+
+atexit.register(print_profile_result)
