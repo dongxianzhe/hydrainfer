24ab0d194a55e8457f903efd84cb2472a8cff73f
Xianzhe Dong <dongxianzhe2019@gmail.com>
2024-11-15 00:24:03 -0800

    [benchmark] add some different prompts to offline inference benchmark used to observe data pattern in model forwarding.

--- benchmark/offline_inference_vision_language_benchmark.py
+++ benchmark/offline_inference_vision_language_benchmark.py
@@ -7,7 +7,14 @@ import os
 image_path = f'./dataset/cherry_blossom.jpg'
 image = Image.open(image_path)
 question = "What is the content of this image?"
+# a prompt with image first
 prompt = f"USER: <image>\n{question}\nASSISTANT:"
+
+# a prompt with image second
+# prompt = f"USER: \n{question}<image>\nASSISTANT:"
+
+# a prompt with image first after decoded
+prompt = f"USER: <image>\n{question}\nASSISTANT:The image features a tall tower with a spire, surrounded by a beautiful cherry blossom tree. The tree is filled with pink flowers, creating a picturesque scene. The tower stands tall in the background, with the blossoming tree in the foreground. The combination of the tower and the tree creates a captivating and serene atmosphere."
 
 def main(args: argparse.Namespace):
     # 1. prepare input
@@ -21,7 +28,7 @@ def main(args: argparse.Namespace):
     # 2. generate
     if args.backend == 'vllm':
         from vllm import LLM, SamplingParams
-        llm = LLM(model="llava-hf/llava-1.5-7b-hf", max_model_len=4096)
+        llm = LLM(model="llava-hf/llava-1.5-7b-hf", max_model_len=4096, enforce_eager=True)
         sampling_params = SamplingParams(temperature=0, max_tokens=50)
 
         start = time.perf_counter()
@@ -30,7 +37,7 @@ def main(args: argparse.Namespace):
 
     elif args.backend == 'dxz':
         from dxz.engine.mllm_engine import MLLMEngine, MLLMEngineConfig
-        config = MLLMEngineConfig()
+        config = MLLMEngineConfig(kvcache_size=6)
         llm = MLLMEngine(config)
 
         start = time.perf_counter()
