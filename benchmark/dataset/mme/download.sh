69eaf29763284f9fcd36ef1d55a29dbc4f5fd7be
Xianzhe Dong <dongxianzhe2019@gmail.com>
2024-11-21 17:28:32 -0800

    [feat] add huggingface lmme-lab mme dataset download shell script.

---
+++ benchmark/dataset/mme/download.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+curl -L -o test-00000-of-00004-a25dbe3b44c4fda6.parquet "https://huggingface.co/datasets/lmms-lab/MME/resolve/main/data/test-00000-of-00004-a25dbe3b44c4fda6.parquet"
+curl -L -o test-00001-of-00004-7d22c7f1aba6fca4.parquet "https://huggingface.co/datasets/lmms-lab/MME/resolve/main/data/test-00001-of-00004-7d22c7f1aba6fca4.parquet"
+curl -L -o test-00002-of-00004-594798fd3f5b029c.parquet "https://huggingface.co/datasets/lmms-lab/MME/resolve/main/data/test-00002-of-00004-594798fd3f5b029c.parquet"
+curl -L -o test-00003-of-00004-53ae1794f93b1e35.parquet "https://huggingface.co/datasets/lmms-lab/MME/resolve/main/data/test-00003-of-00004-53ae1794f93b1e35.parquet"
+
+echo "mme dataset download finished"
