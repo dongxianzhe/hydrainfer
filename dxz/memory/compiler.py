c19d95d363c87330791b04ec909801c6f67f632d
Xianzhe Dong <dongxianzhe2019@gmail.com>
2024-11-24 23:36:56 -0800

    [feat] add random kv cache eviction policy.

--- dxz/memory/compiler.py
+++ dxz/memory/compiler.py
@@ -1,3 +1,4 @@
+import random
 from torch import Tensor
 from dataclasses import dataclass
 from transformers import AutoTokenizer, AutoProcessor
@@ -12,7 +13,7 @@ class CompilerConfig:
     image_token_id: int
     num_image_tokens: int # number of tokens each image embedding
     n_layers: int
-    kv_cache_eviction_policy: Literal['vanilla', 'streamingllm'] = "streamingllm"
+    kv_cache_eviction_policy: Literal['vanilla', 'random', 'streamingllm'] = "random"
     # streamingLLM params
     window_size: int = 12
     attention_sink_size: int = 4
@@ -26,6 +27,8 @@ class Compiler:
         self.num_image_tokens = self.config.num_image_tokens
         if config.kv_cache_eviction_policy == 'vanilla':
             self.code_generator = VanillaCodeGenerator(config)
+        elif config.kv_cache_eviction_policy == 'random':
+            self.code_generator = RandomCodeGenerator(config)
         elif config.kv_cache_eviction_policy == 'streamingllm':
             self.code_generator = StreamingLLMCodeGenerator(config)
 
@@ -201,5 +204,43 @@ class StreamingLLMCodeGenerator:
             )
 
         print(instruction)
+
+        return instruction
+
+class RandomCodeGenerator:
+    def __init__(self, config: CompilerConfig):
+        self.config = config
+        self.window_size = self.config.window_size
+        self.attention_sink_size = self.config.attention_sink_size
+
+    def code_generate(self, token_ids: list[int], pixel_values: list[Tensor]) -> tuple[list[Instruction], int]:
+        instructions: list[Instruction] = []
+        n_virtual_kv_caches = self.config.n_layers
+        virtual_kv_cache_ids = list(range(n_virtual_kv_caches))
+        n_prompt_tokens = len(token_ids)
+        instructions.append(ImageFill(
+            images = pixel_values, 
+            token_ids = token_ids,
+            position_ids = list(range(0, n_prompt_tokens)), 
+            cache_ids = [list(range(0, n_prompt_tokens)) for _ in range(n_virtual_kv_caches)], 
+            kv_cache_ids = virtual_kv_cache_ids, 
+            sample = True, 
+        ))
+
+        return instructions, n_virtual_kv_caches
+
+    def interpret_next_instruction(self, params: DecodeParams) -> Instruction:
+        cache_ids: list[int] = []
+        for _ in range(len(params.curr_instruction.cache_ids)):
+            next_cache_id = random.randint(0, 576)
+            cache_ids.append([next_cache_id])
+
+        instruction = Fill(
+            token_ids = [params.next_token_id],
+            position_ids = [params.curr_instruction.position_ids[-1] + 1], 
+            cache_ids = cache_ids, 
+            kv_cache_ids = params.curr_instruction.kv_cache_ids,
+            sample = True
+            )
 
         return instruction
