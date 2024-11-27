2c31f2a6f26ac3c1dfcc53c66270d61a1a2cb49c
Xianzhe Dong <dongxianzhe2019@gmail.com>
2024-11-27 01:32:12 -0800

    [refactor] encapsulate mme question dict into Question class with dataclasses library.

--- benchmark/mme/MMETestPaper.py
+++ benchmark/mme/MMETestPaper.py
@@ -1,8 +1,59 @@
 import io
 from PIL import Image
 import pandas as pd
+from dataclasses import dataclass
 
+@dataclass
+class Question:
+    question_id: str
+    image: Image
+    question: str
+    reference_answer: str
+    category: str
+    answer: str
 class MMETestPaper:
+    """
+        -----------------------------answer rules-------------------------------
+        total question: 2374
+        each image have two question
+        accuracy   is the number of correct answer divided by the total question
+        accuracy++ is the number of images where both questions are answered correctly divided by the total number of images
+        There are a total of 14 categories, with a maximum score of 2800 points.
+        Each category will have two evaluation metrics: accuracy and accuracy++. Each metric is worth 100 points, for a total of 200 points.
+        total category: 14
+        categorys     : existence, OCR, posters, artwork, numerical_calculation, color, landmark, text_translation, position, count, commonsense_reasoning, scene, celebrity, code_reasoning
+        the total number question each category is:
+            code_reasoning : 40
+            artwork : 400
+            celebrity : 340
+            numerical_calculation : 40
+            text_translation : 40
+            count : 60
+            color : 60
+            commonsense_reasoning : 140
+            position : 60
+            OCR : 40
+            landmark : 400
+            scene : 400
+            existence : 60
+            posters : 294
+        perception score is category score sum of 
+            existence
+            count
+            color
+            position
+            posters
+            celebrity
+            scene
+            landmark
+            artwork
+            OCR
+        cognition score is category score sum of
+            commonsense_reasoning
+            numerical_calculation
+            text_translation
+            code_reasoning
+    """
     def __init__(self):
         files = [
             "test-00000-of-00004-a25dbe3b44c4fda6.parquet", 
@@ -14,62 +65,19 @@ class MMETestPaper:
         for file in files:
             df = pd.read_parquet(file)
             for _, row in df.iterrows():
-                question = {
-                    "question_id": row['question_id'],
-                    "image": Image.open(io.BytesIO(row['image']['bytes'])),
-                    "question": row['question'],
-                    "reference_answer": row['answer'],
-                    "category": row['category'], 
-                    "answer" : "",
-                }
-                self.questions.append(question)
+                self.questions.append(Question(
+                    question_id = row['question_id'],
+                    image = Image.open(io.BytesIO(row['image']['bytes'])),
+                    question = row['question'],
+                    reference_answer = row['answer'],
+                    category = row['category'], 
+                    answer = "",
+                ))
 
-            self.categorys = set([question['category'] for question in self.questions])
+            self.categorys = set([question.category for question in self.questions])
             self.category_count = {}
             for question in self.questions:
-                self.category_count[question['category']] = self.category_count.get(question['category'], 0) + 1
-    
-    def answer_rules(self):
-        print('-----------------------------answer rules-------------------------------')
-        print(f'total question: {len(self.questions)}')
-        print(f'each image have two question')
-        print(f'accuracy   is the number of correct answer divided by the total question')
-        print(f'accuracy++ is the number of images where both questions are answered correctly divided by the total number of images')
-        print(f'There are a total of 14 categories, with a maximum score of 2800 points.')
-        print(f'Each category will have two evaluation metrics: accuracy and accuracy++. Each metric is worth 100 points, for a total of 200 points.')
-        print(f'total category: 14')
-        print(f'categorys     : existence, OCR, posters, artwork, numerical_calculation, color, landmark, text_translation, position, count, commonsense_reasoning, scene, celebrity, code_reasoning')
-        print(f'the total number question each category is:')
-        print(f'    code_reasoning : 40')
-        print(f'    artwork : 400')
-        print(f'    celebrity : 340')
-        print(f'    numerical_calculation : 40')
-        print(f'    text_translation : 40')
-        print(f'    count : 60')
-        print(f'    color : 60')
-        print(f'    commonsense_reasoning : 140')
-        print(f'    position : 60')
-        print(f'    OCR : 40')
-        print(f'    landmark : 400')
-        print(f'    scene : 400')
-        print(f'    existence : 60')
-        print(f'    posters : 294')
-        print(f'perception score is category score sum of ')
-        print(f'    existence')
-        print(f'    count')
-        print(f'    color')
-        print(f'    position')
-        print(f'    posters')
-        print(f'    celebrity')
-        print(f'    scene')
-        print(f'    landmark')
-        print(f'    artwork')
-        print(f'    OCR')
-        print(f'cognition score is category score sum of')
-        print(f'    commonsense_reasoning')
-        print(f'    numerical_calculation')
-        print(f'    text_translation')
-        print(f'    code_reasoning')
+                self.category_count[question.category] = self.category_count.get(question.category, 0) + 1
 
     def mark_paper(self):
         marks = { 
@@ -107,15 +115,15 @@ class MMETestPaper:
             'cognition_score' : 0., 
         }
         for _, (question1, question2) in enumerate(zip(self.questions[::2], self.questions[1::2])):
-            assert question1['question_id'] == question2['question_id']
-            category = question1['category']
+            assert question1.question_id == question2.question_id
+            category = question1.category
             mark1 = category + "_accuracy"
             mark2 = category + "_accuracy++"
-            if question1['answer'] == question1['reference_answer']:
+            if question1.answer == question1.reference_answer:
                 marks[mark1] += 1
-            if question2['answer'] == question2['reference_answer']:
+            if question2.answer == question2.reference_answer:
                 marks[mark1] += 1
-            if question1['answer'] == question1['reference_answer'] and question2['answer'] == question2['reference_answer']:
+            if question1.answer == question1.reference_answer and question2.answer == question2.reference_answer:
                 marks[mark2] += 1
         
         marks['color_accuracy']                   = marks['color_accuracy']                   / (self.category_count['color'])                     * 100
@@ -190,17 +198,11 @@ def test_transformers():
         pretrained_model_name_or_path=model_name_or_path, device_map=device, torch_dtype=torch.half
     )
     processor = AutoProcessor.from_pretrained(pretrained_model_name_or_path=model_name_or_path)
-    inputs = processor(
-        text="USER: <image>\nis this a panda?\nAnswer the question using a single word or phrase. ASSISTANT:", 
-        images=Image.open("/home/xzd/projects/dxz/learn/llava/panda.png"), 
-        return_tensors="pt"
-    )
-
     paper = MMETestPaper()
     for question in tqdm(paper.questions):
         inputs = processor(
-            text=f"USER: <image>\n{question['question']}?\nAnswer the question using a single word or phrase. ASSISTANT:", 
-            images=question['image'],
+            text=f"USER: <image>\n{question.question}?\nAnswer the question using a single word or phrase. ASSISTANT:", 
+            images=question.image,
             return_tensors="pt"
         )
         for key in inputs.keys():
@@ -211,9 +213,9 @@ def test_transformers():
             generated_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False
         )
         if "Yes" in response[0]:
-            question['answer'] = "Yes"
+            question.answer = "Yes"
         elif "No" in response[0]:
-            question['answer'] = "No"
+            question.answer = "No"
         else:
             print('unclear')
     marks = paper.mark_paper()
@@ -224,22 +226,23 @@ def test_engine():
     from dxz.engine.engine import Engine, EngineConfig
     paper = MMETestPaper()
     engine = Engine(EngineConfig(
-        token_prunning_policy='block_prefill',
+        token_prunning_policy='vanilla',
         window_size=512,
         attention_sink_size=4,
     ))
     for question in tqdm(paper.questions):
-        inputs = [{"prompt":f"USER: <image>\n{question['question']}?\nAnswer the question using a single word or phrase. ASSISTANT:", "multi_modal_data":{"image":question['image']}, "max_tokens":16}]
+        inputs = [{"prompt":f"USER: <image>\n{question.question}?\nAnswer the question using a single word or phrase. ASSISTANT:", "multi_modal_data":{"image":question.image}, "max_tokens":16}]
         output_text = engine.generate(inputs)[0]
         print(output_text)
         if "Yes" in output_text:
-            question['answer'] = "Yes"
+            question.answer = "Yes"
         elif "No" in output_text:
-            question['answer'] = "No"
+            question.answer = "No"
         else:
             print('unclear')
     marks = paper.mark_paper()
     print(marks)
 
 if __name__ == '__main__':
-    test_engine()
+    test_transformers()
+    # test_engine()
