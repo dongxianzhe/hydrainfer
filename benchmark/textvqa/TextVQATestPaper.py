3db90821f56d4932484e8222a8a561354019fd73
Xianzhe Dong <dongxianzhe2019@gmail.com>
2024-11-28 00:45:11 -0800

    [benchmark] support text vqa benchmark.

---
+++ benchmark/textvqa/TextVQATestPaper.py
@@ -0,0 +1,293 @@
+import io
+import pandas as pd
+from PIL import Image
+from dataclasses import dataclass
+
+import re
+class EvalAIAnswerProcessor:
+    """
+    Processes an answer similar to Eval AI
+        copied from
+        https://github.com/facebookresearch/mmf/blob/c46b3b3391275b4181567db80943473a89ab98ab/pythia/tasks/processors.py#L897
+    """
+
+    CONTRACTIONS = {
+        "aint": "ain't",
+        "arent": "aren't",
+        "cant": "can't",
+        "couldve": "could've",
+        "couldnt": "couldn't",
+        "couldn'tve": "couldn't've",
+        "couldnt've": "couldn't've",
+        "didnt": "didn't",
+        "doesnt": "doesn't",
+        "dont": "don't",
+        "hadnt": "hadn't",
+        "hadnt've": "hadn't've",
+        "hadn'tve": "hadn't've",
+        "hasnt": "hasn't",
+        "havent": "haven't",
+        "hed": "he'd",
+        "hed've": "he'd've",
+        "he'dve": "he'd've",
+        "hes": "he's",
+        "howd": "how'd",
+        "howll": "how'll",
+        "hows": "how's",
+        "Id've": "I'd've",
+        "I'dve": "I'd've",
+        "Im": "I'm",
+        "Ive": "I've",
+        "isnt": "isn't",
+        "itd": "it'd",
+        "itd've": "it'd've",
+        "it'dve": "it'd've",
+        "itll": "it'll",
+        "let's": "let's",
+        "maam": "ma'am",
+        "mightnt": "mightn't",
+        "mightnt've": "mightn't've",
+        "mightn'tve": "mightn't've",
+        "mightve": "might've",
+        "mustnt": "mustn't",
+        "mustve": "must've",
+        "neednt": "needn't",
+        "notve": "not've",
+        "oclock": "o'clock",
+        "oughtnt": "oughtn't",
+        "ow's'at": "'ow's'at",
+        "'ows'at": "'ow's'at",
+        "'ow'sat": "'ow's'at",
+        "shant": "shan't",
+        "shed've": "she'd've",
+        "she'dve": "she'd've",
+        "she's": "she's",
+        "shouldve": "should've",
+        "shouldnt": "shouldn't",
+        "shouldnt've": "shouldn't've",
+        "shouldn'tve": "shouldn't've",
+        "somebody'd": "somebodyd",
+        "somebodyd've": "somebody'd've",
+        "somebody'dve": "somebody'd've",
+        "somebodyll": "somebody'll",
+        "somebodys": "somebody's",
+        "someoned": "someone'd",
+        "someoned've": "someone'd've",
+        "someone'dve": "someone'd've",
+        "someonell": "someone'll",
+        "someones": "someone's",
+        "somethingd": "something'd",
+        "somethingd've": "something'd've",
+        "something'dve": "something'd've",
+        "somethingll": "something'll",
+        "thats": "that's",
+        "thered": "there'd",
+        "thered've": "there'd've",
+        "there'dve": "there'd've",
+        "therere": "there're",
+        "theres": "there's",
+        "theyd": "they'd",
+        "theyd've": "they'd've",
+        "they'dve": "they'd've",
+        "theyll": "they'll",
+        "theyre": "they're",
+        "theyve": "they've",
+        "twas": "'twas",
+        "wasnt": "wasn't",
+        "wed've": "we'd've",
+        "we'dve": "we'd've",
+        "weve": "we've",
+        "werent": "weren't",
+        "whatll": "what'll",
+        "whatre": "what're",
+        "whats": "what's",
+        "whatve": "what've",
+        "whens": "when's",
+        "whered": "where'd",
+        "wheres": "where's",
+        "whereve": "where've",
+        "whod": "who'd",
+        "whod've": "who'd've",
+        "who'dve": "who'd've",
+        "wholl": "who'll",
+        "whos": "who's",
+        "whove": "who've",
+        "whyll": "why'll",
+        "whyre": "why're",
+        "whys": "why's",
+        "wont": "won't",
+        "wouldve": "would've",
+        "wouldnt": "wouldn't",
+        "wouldnt've": "wouldn't've",
+        "wouldn'tve": "wouldn't've",
+        "yall": "y'all",
+        "yall'll": "y'all'll",
+        "y'allll": "y'all'll",
+        "yall'd've": "y'all'd've",
+        "y'alld've": "y'all'd've",
+        "y'all'dve": "y'all'd've",
+        "youd": "you'd",
+        "youd've": "you'd've",
+        "you'dve": "you'd've",
+        "youll": "you'll",
+        "youre": "you're",
+        "youve": "you've",
+    }
+
+    NUMBER_MAP = {
+        "none": "0",
+        "zero": "0",
+        "one": "1",
+        "two": "2",
+        "three": "3",
+        "four": "4",
+        "five": "5",
+        "six": "6",
+        "seven": "7",
+        "eight": "8",
+        "nine": "9",
+        "ten": "10",
+    }
+    ARTICLES = ["a", "an", "the"]
+    PERIOD_STRIP = re.compile(r"(?!<=\d)(\.)(?!\d)")
+    COMMA_STRIP = re.compile(r"(?<=\d)(\,)+(?=\d)")
+    PUNCTUATIONS = [
+        ";",
+        r"/",
+        "[",
+        "]",
+        '"',
+        "{",
+        "}",
+        "(",
+        ")",
+        "=",
+        "+",
+        "\\",
+        "_",
+        "-",
+        ">",
+        "<",
+        "@",
+        "`",
+        ",",
+        "?",
+        "!",
+    ]
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def word_tokenize(self, word):
+        word = word.lower()
+        word = word.replace(",", "").replace("?", "").replace("'s", " 's")
+        return word.strip()
+
+    def process_punctuation(self, in_text):
+        out_text = in_text
+        for p in self.PUNCTUATIONS:
+            if (p + " " in in_text or " " + p in in_text) or (re.search(self.COMMA_STRIP, in_text) is not None):
+                out_text = out_text.replace(p, "")
+            else:
+                out_text = out_text.replace(p, " ")
+        out_text = self.PERIOD_STRIP.sub("", out_text, re.UNICODE)
+        return out_text
+
+    def process_digit_article(self, in_text):
+        out_text = []
+        temp_text = in_text.lower().split()
+        for word in temp_text:
+            word = self.NUMBER_MAP.setdefault(word, word)
+            if word not in self.ARTICLES:
+                out_text.append(word)
+            else:
+                pass
+        for word_id, word in enumerate(out_text):
+            if word in self.CONTRACTIONS:
+                out_text[word_id] = self.CONTRACTIONS[word]
+        out_text = " ".join(out_text)
+        return out_text
+
+    def __call__(self, item):
+        item = self.word_tokenize(item)
+        item = item.replace("\n", " ").replace("\t", " ").strip()
+        item = self.process_punctuation(item)
+        item = self.process_digit_article(item)
+        return item
+
+@dataclass
+class Question:
+    """
+    https://huggingface.co/datasets/lmms-lab/textvqa/viewer/default/validation
+    we only get a subset columns in the dataset
+    """
+    image_id: str
+    question_id: int
+    question: str
+    image: Image
+    reference_answers: list[str]
+    answer: str
+
+class TextVQATestPaper:
+    def __init__(self):
+        val_files = [
+            "validation-00000-of-00003.parquet", 
+            "validation-00001-of-00003.parquet", 
+            "validation-00002-of-00003.parquet"
+        ]
+        df = pd.read_parquet(val_files)
+        self.questions: list[Question] = []
+        for _, row in df.iterrows():
+            self.questions.append(Question(
+                image_id            = row["image_id"], 
+                question_id         = row["question_id"], 
+                question            = row["question"], 
+                image               = Image.open(io.BytesIO(row['image']['bytes'])), 
+                reference_answers   = list(row["answers"]), 
+                answer              = ""
+            ))
+        assert len(self.questions) == 5000
+    
+    def mark_paper(self):
+        processor = EvalAIAnswerProcessor()
+        marks = {
+            "accuracy":0,
+        }
+        for question in self.questions:
+            question.reference_answers = [processor(ref_ans) for ref_ans in question.reference_answers]
+            question.answer = processor(question.answer)
+            marks['accuracy'] += min(1, question.reference_answers.count(question.answer) / 3)
+        marks['accuracy'] /= len(self.questions)
+        return marks
+
+def test_transformers():
+    from tqdm import tqdm
+    import torch
+    from transformers import LlavaForConditionalGeneration, AutoProcessor
+    device = torch.device('cuda:0')
+    model_name_or_path = "llava-hf/llava-1.5-7b-hf"
+    model = LlavaForConditionalGeneration.from_pretrained(
+        pretrained_model_name_or_path=model_name_or_path, device_map=device, torch_dtype=torch.half
+    )
+    processor = AutoProcessor.from_pretrained(pretrained_model_name_or_path=model_name_or_path)
+    paper = TextVQATestPaper()
+    for question in tqdm(paper.questions):
+        text = f"USER: <image>\n{question.question}?\nWhen the provided information is insufficient, respond with 'Unanswerable'.\nAnswer the question using a single word or phrase. ASSISTANT:"
+        inputs = processor(
+            text=text, 
+            images=question.image,
+            return_tensors="pt"
+        )
+        for key in inputs.keys():
+            inputs[key] = inputs[key].to(device)
+        generated_ids = model.generate(**inputs, max_new_tokens=15)
+
+        question.answer = processor.batch_decode(
+            generated_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False
+        )[0].split('ASSISTANT:')[1].strip()
+
+    marks = paper.mark_paper()
+    print(marks)
+    
+if __name__ == '__main__':
+    test_transformers()
---
+++ benchmark/textvqa/download.sh
@@ -0,0 +1,5 @@
+curl -L -o validation-00000-of-00003.parquet "https://huggingface.co/datasets/lmms-lab/textvqa/resolve/main/data/validation-00000-of-00003.parquet"
+curl -L -o validation-00001-of-00003.parquet "https://huggingface.co/datasets/lmms-lab/textvqa/resolve/main/data/validation-00001-of-00003.parquet"
+curl -L -o validation-00002-of-00003.parquet "https://huggingface.co/datasets/lmms-lab/textvqa/resolve/main/data/validation-00002-of-00003.parquet"
+
+echo "text_vqa dataset download finished"
