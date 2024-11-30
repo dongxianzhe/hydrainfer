06ad0fbc308d8d02d43ba1b75815146e181a723c
Xianzhe Dong <dongxianzhe2019@gmail.com>
2024-11-30 01:53:24 -0800

    [ut] test flashinfer batch decode interface.

--- tests/kernel/test_attention_kernel.py
+++ tests/kernel/test_attention_kernel.py
@@ -1,3 +1,5 @@
+import random
+import flashinfer
 import copy
 from itertools import accumulate
 import math
@@ -5,6 +7,7 @@ import torch
 from torch import Tensor
 import pytest
 from flash_attn import flash_attn_func, flash_attn_qkvpacked_func, flash_attn_with_kvcache, flash_attn_kvpacked_func
+from dxz._C.kernel.flash_attn import mha_varlen_fwd
 from dxz.memory.kv_cache import KVCache
 from dxz.memory.block_allocator import BlockAllocator
 from dxz.layer.attention import TorchCausalGroupedQueryPageAttention
@@ -231,7 +234,79 @@ def test_fixedlength_nokvcache_attention(
     # assert torch.allclose(kv_cache.key_cache, kv_cache_ref.key_cache, rtol=1e-3, atol=1e-3)
     # assert torch.allclose(kv_cache.value_cache, kv_cache_ref.value_cache, rtol=1e-3, atol=1e-3)
 
+@pytest.mark.parametrize("batch_size", [1, 2, 3, 4, 8])
+@pytest.mark.parametrize("num_heads", [(8, 8), (8, 4), (8, 1)])
+@pytest.mark.parametrize("head_dim", [64, 128, 256])
+@pytest.mark.parametrize("causal", [True])
+@pytest.mark.parametrize("dtype", [torch.float16])
+@pytest.mark.parametrize("device", [torch.device('cuda:0')])
+@torch.inference_mode()
+def test_flash_infer_batch_decode(
+    batch_size: int, 
+    num_heads: tuple[int, int], 
+    head_dim: int, 
+    causal: bool,
+    dtype: torch.dtype,
+    device: torch.device,
+):
+    n_tokens = batch_size
+    num_qo_heads, num_kv_heads = num_heads
+    total_blocks = max_num_blocks = max_num_pages = 128
+    block_size = page_size = 16
+
+    q = torch.randn(size=(batch_size, num_qo_heads, head_dim), dtype=dtype, device=device)
+    block_tables = kv_page_indices = torch.randperm(total_blocks, dtype=torch.int, device=device)
+    q_cu_seq_lens = torch.arange(batch_size + 1, dtype=torch.int, device=device)
+    k_seq_lens = [random.randint(1, 10) for _ in range(batch_size)]
+    k_cu_seq_lens = torch.tensor(list(accumulate(k_seq_lens, initial=0)), dtype=torch.int, device=device)
+    cu_block_lens = kv_page_indptr = torch.tensor(list(accumulate([(k_seq_len + block_size - 1) // block_size for k_seq_len in k_seq_lens], initial=0)), dtype=torch.int32, device=device)
+    kv_last_page_len = torch.tensor([k_seq_len % page_size for k_seq_len in k_seq_lens], dtype=torch.int, device=device)
+    kvcache = KVCache(max_num_pages, page_size, num_kv_heads, head_dim, dtype, device)
+    k_cache = torch.randn(size=(max_num_pages, page_size, num_kv_heads, head_dim), dtype=dtype, device=device)
+    v_cache = torch.randn(size=(max_num_pages, page_size, num_kv_heads, head_dim), dtype=dtype, device=device)
+
+    workspace_buffer = torch.empty(128 * 1024 * 1024, dtype=torch.uint8, device=device)
+    decode_wrapper = flashinfer.BatchDecodeWithPagedKVCacheWrapper(workspace_buffer, "NHD")
+    decode_wrapper.plan(
+        kv_page_indptr,
+        kv_page_indices,
+        kv_last_page_len,
+        num_qo_heads,
+        num_kv_heads,
+        head_dim,
+        page_size,
+        pos_encoding_mode="NONE",
+        data_type=dtype
+    )
+
+    o = decode_wrapper.run(q, (k_cache, v_cache))
+
+    o_ref = torch.zeros(size=(batch_size, num_qo_heads, head_dim), dtype=dtype, device=device)
+    mha_varlen_fwd(
+        o_ref, 
+        q,
+        k_cache,
+        v_cache,
+        q_cu_seq_lens, 
+        k_cu_seq_lens, 
+        block_tables, 
+        cu_block_lens, 
+        None, 
+        1, 
+        max(k_seq_lens), 
+        1. / math.sqrt(head_dim), 
+        0.,
+        -1, 
+        0, 
+        0, 
+    )
+
+    print(f'o.view(-1)[:8] {o.view(-1)[:8]}')
+    print(f'o_ref.view(-1)[:8] {o_ref.view(-1)[:8]}')
+    assert torch.allclose(o, o_ref, atol=1e-3, rtol=1e-3)
+
 if __name__ == '__main__':
-    pytest.main([__file__, '-x'])
+    # pytest.main([__file__, '-x'])
+    pytest.main([__file__ + '::test_flash_infer_batch_decode', '-x'])
     # pytest.main([__file__ + '::test_variable_length_attention', '-x'])
     # pytest.main([__file__, '-s']) # print
