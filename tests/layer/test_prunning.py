a91da297a15b30e4bfc4df842ec5c1ca5f27194b
Xianzhe Dong <dongxianzhe2019@gmail.com>
2024-11-30 23:09:18 -0800

    [ut] test focal prunning method.

---
+++ tests/layer/test_prunning.py
@@ -0,0 +1,31 @@
+import torch
+from dxz.layer.token_prunning import FocalPrunning
+
+def test_focal_prunning():
+    model = FocalPrunning(2 / 3, 'rank')
+    batch_size = 2
+    n_tokens = 3
+    hidden_size = 1024
+    n_heads = 2
+    dtype = torch.half
+    device = torch.device('cuda:0')
+    tokens = torch.randn(size=(batch_size, n_tokens, hidden_size), dtype=dtype, device=device)
+
+    scores = [[
+        [-2, 5, 5],
+        [-7, 3, 8],
+        [-7, -7, -8],
+        ],[
+        [6, 9, 8],  
+        [3, 10, 7], 
+        [2, 9, -9],
+    ]]
+    scores = torch.tensor(scores, dtype=dtype, device=device)
+
+    print(f'tokens.shape {tokens.shape}')
+    o = model(tokens, scores)
+    print(f'tokens.shape {tokens.shape}')
+    assert torch.allclose(o, tokens[:, :2, :])
+
+if __name__ == '__main__':
+    test_focal_prunning()
