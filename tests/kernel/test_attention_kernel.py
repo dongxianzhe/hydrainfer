951983787319541445197a68516d6a86e8042968
Xianzhe Dong <dongxianzhe2019@gmail.com>
2024-11-30 17:23:21 -0800

    [ut] test flash infer batch prefill interface.

--- tests/kernel/test_attention_kernel.py
+++ tests/kernel/test_attention_kernel.py
@@ -13,23 +13,6 @@ from dxz.memory.block_allocator import BlockAllocator
 from dxz.layer.attention import TorchCausalGroupedQueryPageAttention
 from dxz.model.parameters import InputParameters
 
-"""
-if q fixed length:
-    if k fixed length(no kvcache):
-        flash_attn_qkvpacked_func
-        flash_attn_kvpacked_func
-        flash_attn_func
-    elif kvcache:
-        flash_attn_with_kvcache
-elif q variable length:
-    if k variable length(nokvcache):
-        flash_attn_varlen_qkvpacked_func
-        flash_attn_varlen_kvpacked_func
-        flash_attn_varlen_func
-    elif kvcache:
-        flash_attn_with_kvcache
-"""
-
 @pytest.mark.parametrize("batch_size", [1, 2, 3, 4, 8])
 @pytest.mark.parametrize("seq_len", [16, 576, 577])
 @pytest.mark.parametrize("num_heads", [(8, 8), (8, 4), (8, 1)])
@@ -38,7 +21,7 @@ elif q variable length:
 @pytest.mark.parametrize("dtype", [torch.float16])
 @pytest.mark.parametrize("device", [torch.device('cuda:0')])
 @torch.inference_mode()
-def test_fixedlength_nokvcache_attention(
+def test_flash_attn_func(
     batch_size: int, 
     seq_len: int, 
     num_heads: tuple[int, int], 
@@ -120,120 +103,6 @@ def test_fixedlength_nokvcache_attention(
     )
     assert torch.allclose(flash_attn_kvpacked_func_out, out_ref, rtol=1e-3, atol=1e-3)
 
-# @pytest.mark.parametrize("seq_lens", [4, 16, 576, 577])
-# @pytest.mark.parametrize("num_heads", [(8, 8), (8, 4), (8, 1)])
-# @pytest.mark.parametrize("head_dim", [64, 128, 256])
-# @pytest.mark.parametrize("causal", [False, True])
-# @pytest.mark.parametrize("dtype", [torch.float16])
-# @pytest.mark.parametrize("device", [torch.device('cuda:0')])
-# @torch.inference_mode()
-# def test_varlength_nokvcache_attention(
-#     seq_lens: list[int], 
-#     num_heads: tuple[int, int], 
-#     head_dim: int, 
-#     causal: bool,
-#     dtype: torch.dtype,
-#     device: torch.device,
-# ):
-#     group_size = num_heads[0] // num_heads[1]
-
-#     sum_seq_lens = sum(seq_lens)
-#     q = torch.randn(size=(sum_seq_lens, num_heads[0], head_dim), dtype=dtype, device=device)
-#     k = torch.randn(size=(sum_seq_lens, num_heads[1], head_dim), dtype=dtype, device=device)
-#     v = torch.randn(size=(sum_seq_lens, num_heads[1], head_dim), dtype=dtype, device=device)
-
-# @pytest.mark.parametrize("seq_lens", [[(1, 100), (15, 15), (111, 234), (1000, 10000)]])
-# @pytest.mark.parametrize("num_heads", [(8, 8), (8, 4), (8, 2), (8, 1)])
-# @pytest.mark.parametrize("head_size", [64, 128, 256])
-# @pytest.mark.parametrize("n_blocks", [100])
-# @pytest.mark.parametrize("block_size", [4, 8, 16])
-# @pytest.mark.parametrize("dtype", [torch.float16, torch.bfloat16])
-# @pytest.mark.parametrize("logits_soft_cap", [0.0, 50.0])
-# @pytest.mark.parametrize("sliding_window", [-1, 50])
-# @pytest.mark.parametrize("alibi", [False, True])
-
-# @pytest.mark.parametrize("seq_lens", [[(1, 100), (1, 15), (1, 234), (1, 10000)]])
-# @pytest.mark.parametrize("num_heads", [(8, 8)])
-# @pytest.mark.parametrize("head_size", [64])
-# @pytest.mark.parametrize("n_blocks", [100])
-# @pytest.mark.parametrize("block_size", [16])
-# @pytest.mark.parametrize("dtype", [torch.float16])
-# @pytest.mark.parametrize("device", [torch.device('cuda:0')])
-# @torch.inference_mode()
-# def test_fixedlength_kvcache_attention(
-#     seq_lens  : list[tuple[int, int]], # each tuple is a sequence
-#     num_heads : list[tuple[int]], # q heads and kv heads
-#     head_size : int, 
-#     n_blocks  : int,
-#     block_size: int,
-#     dtype     : torch.dtype, 
-#     device    : torch.device,
-# ):
-#     q_seq_lens = [x for x, _ in seq_lens]
-
-#     sum_q_seq_len = sum(q_seq_lens)
-#     kv_seq_lens = [x for _, x in seq_lens]
-#     block_lens = [(kv_seq_len + block_size - 1) // block_size for kv_seq_len in kv_seq_lens]
-#     allocator = BlockAllocator(n_blocks)
-#     block_tables: list[list[int]] = []
-#     for block_len in block_lens:
-#         block_tables.append(allocator.allocate(block_len))
-
-#     q = torch.randn(size=(sum_q_seq_len, num_heads[0], head_size), dtype=dtype, device=device)
-#     k = torch.randn(size=(sum_q_seq_len, num_heads[1], head_size), dtype=dtype, device=device)
-#     v = torch.randn(size=(sum_q_seq_len, num_heads[1], head_size), dtype=dtype, device=device)
-#     new_cache_slots = torch.randperm(n_blocks * block_size, dtype=torch.int, device=device)[: sum_q_seq_len], 
-#     kv_cache = KVCache(num_blocks=n_blocks, block_size=block_size, num_kv_heads=num_heads[1], head_size=head_size, dtype=dtype, device=device)
-#     kv_cache_ref = copy.deepcopy(kv_cache)
-
-
-#     input_parmas = InputParameters(
-#         num_sequences = len(seq_lens), 
-#         q_cu_seq_lens = torch.tensor(list(accumulate(q_seq_lens, initial=0)), dtype=torch.int, device=device), 
-#         kv_cu_seq_lens = torch.tensor(list(accumulate(kv_seq_lens, initial=0)), dtype=torch.int, device=device), 
-#         new_cache_slots = new_cache_slots, 
-#         block_tables = torch.tensor(list(range(n_blocks)), dtype=torch.int, device=device), 
-#         cu_blocks_lens = torch.tensor(list(accumulate(block_lens, initial=0)), dtype=torch.int, device=device), 
-#         q_max_seq_len = 1024, 
-#         kv_max_seq_len = 1024, 
-#     )
-#     ref_attention = TorchCausalGroupedQueryPageAttention(
-#         n_qo_heads = num_heads[0], 
-#         n_kv_heads = num_heads[1], 
-#         head_dim   = head_size, 
-#     )
-
-#     assert torch.allclose(kv_cache.key_cache, kv_cache_ref.key_cache, rtol=1e-3, atol=1e-3)
-#     assert torch.allclose(kv_cache.value_cache, kv_cache_ref.value_cache, rtol=1e-3, atol=1e-3)
-#     out_ref = ref_attention(q, k, v, kv_cache_ref, input_parmas)
-#     assert not torch.allclose(kv_cache.key_cache, kv_cache_ref.key_cache, rtol=1e-3, atol=1e-3)
-#     assert not torch.allclose(kv_cache.value_cache, kv_cache_ref.value_cache, rtol=1e-3, atol=1e-3)
-
-    # flash_attn_with_kvcache_out = flash_attn_with_kvcache(
-    #     q = q,
-    #     k_cache = kv_cache.key_cache,
-    #     v_cache = kv_cache.value_cache,
-    #     k = k,
-    #     v = v,
-    #     rotary_cos = None,
-    #     rotary_sin = None,
-    #     cache_seqlens = torch.tensor(kv_seq_lens, dtype=torch.int, device=device), 
-    #     cache_batch_idx = new_cache_slots, 
-    #     cache_leftpad = None,
-    #     block_table: Optional[torch.Tensor] = None,
-    #     softmax_scale=None,
-    #     causal=False,
-    #     window_size=(-1, -1),  # -1 means infinite context window
-    #     softcap=0.0, # 0.0 means deactivated
-    #     rotary_interleaved=True,
-    #     alibi_slopes=None,
-    #     num_splits=0,
-    #     return_softmax_lse=False,
-    # )
-
-    # assert torch.allclose(kv_cache.key_cache, kv_cache_ref.key_cache, rtol=1e-3, atol=1e-3)
-    # assert torch.allclose(kv_cache.value_cache, kv_cache_ref.value_cache, rtol=1e-3, atol=1e-3)
-
 @pytest.mark.parametrize("batch_size", [1, 2, 3, 4, 8])
 @pytest.mark.parametrize("num_heads", [(8, 8), (8, 4), (8, 1)])
 @pytest.mark.parametrize("head_dim", [64, 128, 256])
@@ -257,10 +126,11 @@ def test_flash_infer_batch_decode(
     q = torch.randn(size=(batch_size, num_qo_heads, head_dim), dtype=dtype, device=device)
     block_tables = kv_page_indices = torch.randperm(total_blocks, dtype=torch.int, device=device)
     q_cu_seq_lens = torch.arange(batch_size + 1, dtype=torch.int, device=device)
-    k_seq_lens = [random.randint(1, 10) for _ in range(batch_size)]
+    k_seq_lens = [random.randint(1, 256) for _ in range(batch_size)]
     k_cu_seq_lens = torch.tensor(list(accumulate(k_seq_lens, initial=0)), dtype=torch.int, device=device)
     cu_block_lens = kv_page_indptr = torch.tensor(list(accumulate([(k_seq_len + block_size - 1) // block_size for k_seq_len in k_seq_lens], initial=0)), dtype=torch.int32, device=device)
-    kv_last_page_len = torch.tensor([k_seq_len % page_size for k_seq_len in k_seq_lens], dtype=torch.int, device=device)
+    kv_last_page_len = torch.tensor([page_size if k_seq_len % page_size == 0 else k_seq_len % page_size for k_seq_len in k_seq_lens], dtype=torch.int, device=device)
+
     kvcache = KVCache(max_num_pages, page_size, num_kv_heads, head_dim, dtype, device)
     k_cache = torch.randn(size=(max_num_pages, page_size, num_kv_heads, head_dim), dtype=dtype, device=device)
     v_cache = torch.randn(size=(max_num_pages, page_size, num_kv_heads, head_dim), dtype=dtype, device=device)
@@ -305,8 +175,79 @@ def test_flash_infer_batch_decode(
     print(f'o_ref.view(-1)[:8] {o_ref.view(-1)[:8]}')
     assert torch.allclose(o, o_ref, atol=1e-3, rtol=1e-3)
 
+@pytest.mark.parametrize("batch_size", [1, 2, 3, 4, 8])
+@pytest.mark.parametrize("num_heads", [(8, 8), (8, 4), (8, 1)])
+@pytest.mark.parametrize("head_dim", [64, 128, 256])
+@pytest.mark.parametrize("causal", [True])
+@pytest.mark.parametrize("dtype", [torch.float16])
+@pytest.mark.parametrize("device", [torch.device('cuda:0')])
+@torch.inference_mode()
+def test_flash_infer_batch_prefill(
+    batch_size: int, 
+    num_heads: tuple[int, int], 
+    head_dim: int, 
+    causal: bool,
+    dtype: torch.dtype,
+    device: torch.device,
+):
+    import flashinfer
+    num_qo_heads, num_kv_heads = num_heads
+    total_blocks = max_num_blocks = max_num_pages = 128
+    block_size = page_size = 16
+
+    q_seq_lens = [random.randint(1, 20) for _ in range(batch_size)]
+    q_cu_seq_lens = qo_indptr = torch.tensor(list(accumulate(q_seq_lens, initial=0)), dtype=torch.int, device=device)
+    k_seq_lens = [random.randint(1, 256) for _ in range(batch_size)]
+    k_cu_seq_lens = torch.tensor(list(accumulate(k_seq_lens, initial=0)), dtype=torch.int, device=device)
+    paged_kv_last_page_len = torch.tensor([page_size if k_seq_len % page_size == 0 else k_seq_len % page_size for k_seq_len in k_seq_lens], dtype=torch.int32, device=device)
+
+    block_tables = paged_kv_indices = torch.arange(max_num_pages, dtype=torch.int, device=device)
+    cu_block_lens = paged_kv_indptr = torch.tensor(list(accumulate([((k_seq_len + block_size - 1) // block_size) for k_seq_len in k_seq_lens], initial=0)), dtype=torch.int32, device=device)
+
+    q = torch.randn(size=(sum(q_seq_lens), num_qo_heads, head_dim), dtype=dtype, device=device)
+    k_cache = torch.randn(size=(max_num_pages, page_size, num_kv_heads, head_dim), dtype=dtype, device=device)
+    v_cache = torch.randn(size=(max_num_pages, page_size, num_kv_heads, head_dim), dtype=dtype, device=device)
+
+    workspace_buffer = torch.empty(128 * 1024 * 1024, dtype=torch.uint8, device=device)
+    prefill_wrapper = flashinfer.BatchPrefillWithPagedKVCacheWrapper(workspace_buffer, "NHD")
+    prefill_wrapper.plan(
+        qo_indptr,
+        paged_kv_indptr,
+        paged_kv_indices,
+        paged_kv_last_page_len,
+        num_qo_heads,
+        num_kv_heads,
+        head_dim,
+        page_size,
+        causal=causal,
+    )
+
+    o = prefill_wrapper.run(q, (k_cache, v_cache))
+    o_ref = torch.zeros(size=(sum(q_seq_lens), num_qo_heads, head_dim), dtype=dtype, device=device)
+    mha_varlen_fwd(
+        o_ref, 
+        q, 
+        k_cache, 
+        v_cache, 
+        q_cu_seq_lens, 
+        k_cu_seq_lens, 
+        block_tables, 
+        cu_block_lens, 
+        None, 
+        max(q_seq_lens), 
+        max(k_seq_lens), 
+        1. / math.sqrt(head_dim), 
+        0., 
+        -1, 
+        0, 
+        0, 
+    )
+
+    print(f'o.view(-1)[:8] {o.view(-1)[:8]}')
+    print(f'o_ref.view(-1)[:8] {o_ref.view(-1)[:8]}')
+    assert torch.allclose(o, o_ref, atol=1e-3, rtol=1e-3)
+
 if __name__ == '__main__':
-    # pytest.main([__file__, '-x'])
+    pytest.main([__file__ + '::test_flash_attn_func', '-x'])
     pytest.main([__file__ + '::test_flash_infer_batch_decode', '-x'])
-    # pytest.main([__file__ + '::test_variable_length_attention', '-x'])
-    # pytest.main([__file__, '-s']) # print
+    pytest.main([__file__ + '::test_flash_infer_batch_prefill', '-x'])
