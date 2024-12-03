874d5ed115d2ab006d689d4011906d7261e06942
Xianzhe Dong <dongxianzhe2019@gmail.com>
2024-12-02 18:48:32 -0800

    [ut] test flash infer batch prefill mask.

--- tests/kernel/test_attention_kernel.py
+++ tests/kernel/test_attention_kernel.py
@@ -247,7 +247,156 @@ def test_flash_infer_batch_prefill(
     print(f'o_ref.view(-1)[:8] {o_ref.view(-1)[:8]}')
     assert torch.allclose(o, o_ref, atol=1e-3, rtol=1e-3)
 
+@pytest.mark.parametrize("batch_size", [1, 2, 4, 8])
+@pytest.mark.parametrize("num_heads", [(8, 8), (8, 4), (8, 1)])
+@pytest.mark.parametrize("head_dim", [64, 128, 256])
+@pytest.mark.parametrize("dtype", [torch.float16])
+@pytest.mark.parametrize("device", [torch.device('cuda:0')])
+@pytest.mark.parametrize("mask_type", ["causal", "random", "streamingllm"])
+@torch.inference_mode()
+def test_flash_infer_batch_prefill_mask(
+    batch_size: int, 
+    num_heads : tuple[int, int], 
+    head_dim  : int,
+    dtype     : torch.dtype, 
+    device    : torch.device,
+    mask_type : str, 
+):
+    import flashinfer
+    num_qo_heads, num_kv_heads = num_heads
+    total_blocks = max_num_blocks = max_num_pages = 128
+    block_size = page_size = 16
+    num_sequences = batch_size
+
+    q_seq_lens = [random.randint(1, 20) for _ in range(batch_size)]
+    q_cu_seq_lens = qo_indptr = torch.tensor(list(accumulate(q_seq_lens, initial=0)), dtype=torch.int, device=device)
+    k_seq_lens = [random.randint(21, 256) for _ in range(batch_size)]
+    k_cu_seq_lens = torch.tensor(list(accumulate(k_seq_lens, initial=0)), dtype=torch.int, device=device)
+    paged_kv_last_page_len = torch.tensor([page_size if k_seq_len % page_size == 0 else k_seq_len % page_size for k_seq_len in k_seq_lens], dtype=torch.int32, device=device)
+    
+    block_tables = paged_kv_indices = torch.arange(max_num_pages, dtype=torch.int, device=device)
+    cu_block_lens = paged_kv_indptr = torch.tensor(list(accumulate([((k_seq_len + block_size - 1) // block_size) for k_seq_len in k_seq_lens], initial=0)), dtype=torch.int32, device=device)
+    
+    q = torch.randn(size=(sum(q_seq_lens), num_qo_heads, head_dim), dtype=dtype, device=device)
+    k_cache = torch.randn(size=(max_num_pages, page_size, num_kv_heads, head_dim), dtype=dtype, device=device)
+    v_cache = torch.randn(size=(max_num_pages, page_size, num_kv_heads, head_dim), dtype=dtype, device=device)
+
+    masks = []
+    if mask_type == 'causal':
+        # causal mask
+        for q_seq_len, k_seq_len in zip(q_seq_lens, k_seq_lens):
+            mask = torch.ones(q_seq_len, k_seq_len, dtype=torch.bool, device=q.device)
+            mask = mask.tril(diagonal=k_seq_len - q_seq_len)
+            masks.append(mask.reshape(-1))
+    elif mask_type == 'random':
+        # random mask
+        for q_seq_len, k_seq_len in zip(q_seq_lens, k_seq_lens):
+            mask = torch.randint(0, 2, (q_seq_len, k_seq_len), dtype=torch.bool, device=device)
+            masks.append(mask.reshape(-1))
+    elif mask_type == 'streamingllm':
+        attention_sink = 1
+        window_size = 3
+        for i, (q_seq_len, k_seq_len) in enumerate(zip(q_seq_lens, k_seq_lens)):
+            mask = torch.ones(size=(q_seq_len, k_seq_len), dtype=torch.bool, device=device)
+            mask = torch.triu(mask, diagonal=k_seq_len - q_seq_len - window_size)
+            mask = torch.tril(mask, diagonal=k_seq_len - q_seq_len)
+            mask[:, :attention_sink] = 1
+
+            mask = torch.randint(0, 2, (q_seq_len, k_seq_len), dtype=torch.bool, device=device)
+            masks.append(mask.reshape(-1))
+
+    
+    custom_mask = torch.cat(masks, dim=0)
+
+    workspace_buffer = torch.empty(128 * 1024 * 1024, dtype=torch.uint8, device=device)
+    prefill_wrapper = flashinfer.BatchPrefillWithPagedKVCacheWrapper(workspace_buffer, "NHD", use_cuda_graph=False)
+    prefill_wrapper.plan(
+        qo_indptr              = qo_indptr,
+        paged_kv_indptr        = paged_kv_indptr,
+        paged_kv_indices       = paged_kv_indices,
+        paged_kv_last_page_len = paged_kv_last_page_len,
+        num_qo_heads           = num_qo_heads,
+        num_kv_heads           = num_kv_heads,
+        head_dim               = head_dim,
+        page_size              = page_size,
+        # causal                 = True,
+        custom_mask            = custom_mask, 
+    )
+    o = prefill_wrapper.run(q, (k_cache, v_cache))
+    
+    def ref_batch_prefill(
+        n_qo_heads: int, 
+        n_kv_heads: int, 
+        head_dim: int, 
+        query, 
+        k_cache: Tensor, 
+        v_cache: Tensor, 
+        num_sequences: int, 
+        q_cu_seq_lens: Tensor, 
+        cu_block_lens: Tensor, 
+        kv_cu_seq_lens: Tensor, 
+        custom_mask: Tensor, 
+        ) -> Tensor:
+        outputs = []
+        cu_mask_len: int = 0
+        for i in range(num_sequences):
+            block_table = block_tables[cu_block_lens[i]: cu_block_lens[i + 1]]
+            key = k_cache[block_table, :, :, :].reshape(-1, n_kv_heads, head_dim)
+            value = v_cache[block_table, :, :, :].reshape(-1, n_kv_heads, head_dim)
+            k = key[: kv_cu_seq_lens[i + 1] - kv_cu_seq_lens[i], :, :].to(torch.float)
+            v = value[: kv_cu_seq_lens[i + 1] - kv_cu_seq_lens[i],:, :].to(torch.float)
+            q = query[q_cu_seq_lens[i]: q_cu_seq_lens[i + 1], :, :].to(torch.float)
+            # q (qo_seq_len, n_qo_heads, head_dim)
+            # k (kv_seq_len, n_kv_heads, head_dim)
+            # v (kv_seq_len, n_kv_heads, head_dim)
+            group_size = n_qo_heads // n_kv_heads
+            k = k.repeat_interleave(repeats=group_size, dim=1)
+            v = v.repeat_interleave(repeats=group_size, dim=1)
+            # k (kv_seq_len, n_qo_heads, head_dim)
+            # v (kv_seq_len, n_qo_heads, head_dim)
+
+            # compute score
+            scores = torch.einsum('qhd,khd->hqk', q, k)
+
+            # sm_scale
+            sm_scale = 1. / math.sqrt(head_dim)
+            scores *= sm_scale
+
+            # mask
+            _, q_seq_len, k_seq_len = scores.shape
+            mask = custom_mask[cu_mask_len: cu_mask_len + q_seq_len * k_seq_len].reshape(q_seq_len, k_seq_len)
+            cu_mask_len += q_seq_len * k_seq_len
+            scores.masked_fill_(mask=~mask, value=float('-inf'))
+
+            # softmax
+            scores = torch.softmax(scores, dim=-1)
+            o = torch.einsum("hqk,khd->qhd", scores, v)
+            outputs.append(o)
+
+        output = torch.cat(outputs, dim=0)
+        return output.to(query.dtype)
+
+    o_ref = ref_batch_prefill(
+        n_qo_heads = num_qo_heads, 
+        n_kv_heads = num_kv_heads, 
+        head_dim = head_dim, 
+        query = q, 
+        k_cache = k_cache,
+        v_cache = v_cache,
+        num_sequences = batch_size, 
+        q_cu_seq_lens = q_cu_seq_lens, 
+        cu_block_lens = cu_block_lens, 
+        kv_cu_seq_lens = k_cu_seq_lens, 
+        custom_mask = custom_mask, 
+    )
+
+    print(f'o.view(-1)[:8] {o.view(-1)[:]}')
+    print(f'o_ref.view(-1)[:8] {o_ref.view(-1)[:]}')
+    assert torch.allclose(o, o_ref, atol=1e-1, rtol=1e-1)
+
+
 if __name__ == '__main__':
     pytest.main([__file__ + '::test_flash_attn_func', '-x'])
     pytest.main([__file__ + '::test_flash_infer_batch_decode', '-x'])
     pytest.main([__file__ + '::test_flash_infer_batch_prefill', '-x'])
+    pytest.main([__file__ + '::test_flash_infer_batch_prefill_mask', '-x'])
