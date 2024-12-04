0a8f20ccc8c2c06ed743792033bbc7bea83bdaa4
Xianzhe Dong <dongxianzhe2019@gmail.com>
2024-12-03 21:03:32 -0800

    [feat] test relation between batch size and throughput of decode and image embed.

--- experiments/test_batch_embed_prefill_decode.py
+++ experiments/test_batch_embed_prefill_decode.py
@@ -84,6 +84,96 @@ def plot_prefill_batchsize_throughput():
     bar_chart(batch_sizes, throughputs, figsize=(8, 6), xlabel="Batch Size", ylabel="Tokens per Second", title="Prefill", filename="bs2throughput")
 
 # 2. test decode
+def decode_batchsize_throughput(batch_size) -> float:
+    prompt_length = 512
+    num_sequences = batch_size
+    total_blocks = 2048
+    block_size = 16
+    n_tokens = 1 * num_sequences
+    input_ids = torch.zeros(size=(n_tokens, ), dtype=torch.int, device=device)
+    position_ids = torch.zeros(size=(n_tokens, ), dtype=torch.int, device=device)
+    kv_cache = KVCache(total_blocks, block_size, num_kv_heads, head_size, dtype, device)
+    q_seq_lens = [1 for _ in range(num_sequences)]
+    q_cu_seq_lens = torch.tensor(list(accumulate(q_seq_lens, initial=0)), dtype=torch.int, device=device)
+    k_seq_lens = [prompt_length for _ in range(num_sequences)]
+    kv_cu_seq_lens = torch.tensor(list(accumulate(k_seq_lens, initial=0)), dtype=torch.int, device=device)
+    new_cache_slots = torch.arange(num_sequences, dtype=torch.int, device=device)
+
+    n_blocks = (prompt_length + block_size - 1) // block_size
+    block_lens = [n_blocks for _ in range(num_sequences)]
+    block_tables = torch.arange(n_blocks, dtype=torch.int, device=device)[None, :].repeat(num_sequences, 1).reshape(-1)
+    cu_blocks_lens = torch.tensor(list(accumulate(block_lens, initial=0)), dtype=torch.int, device=device)
+    q_max_seq_len = max(q_seq_lens)
+    kv_max_seq_len = max(k_seq_lens)
+    model_params = ModelParameters(
+        attention_params = [AttentionParameters(
+            kv_cache=kv_cache,
+            q_cu_seq_lens = q_cu_seq_lens, 
+            kv_cu_seq_lens = kv_cu_seq_lens, 
+            new_cache_slots = new_cache_slots, 
+            block_tables = block_tables, 
+            cu_blocks_lens = cu_blocks_lens, 
+            num_sequences = num_sequences, 
+            all_sequences_decode = False, 
+            q_max_seq_len = q_max_seq_len,
+            kv_max_seq_len = kv_max_seq_len, 
+        )for layer_id in range(n_layers)]
+    )
+    logits = model(input_ids=input_ids, pixel_values=None, image_features=None, position_ids=position_ids, model_params=model_params)
+    logits = model(input_ids=input_ids, pixel_values=None, image_features=None, position_ids=position_ids, model_params=model_params)
+    logits = model(input_ids=input_ids, pixel_values=None, image_features=None, position_ids=position_ids, model_params=model_params)
+
+    start = time.perf_counter()
+    num_repeats = 10
+    for i in range(num_repeats):
+        logits = model(input_ids=input_ids, pixel_values=None, image_features=None, position_ids=position_ids, model_params=model_params)
+        torch.cuda.synchronize()
+    end = time.perf_counter()
+    duration = end - start
+    throughput = num_sequences * num_repeats / duration
+    return throughput 
+
+def plot_decode_batchsize_throughput():
+    batch_sizes = [i for i in range(1, 64)]
+    throughputs = []
+    for batch_size in batch_sizes:
+        throughput = decode_batchsize_throughput(batch_size)
+        print(f"batch_size {batch_size} throughput {throughput} tokens / s")
+        throughputs.append(throughput)
+
+    bar_chart(batch_sizes, throughputs, figsize=(8, 6), xlabel="Batch Size", ylabel="Tokens per Second", title="Decode", filename="decodebs2throughput")
+
+def embed_batchsize_throughput(batch_size) -> float:
+    model_params = ModelParameters()
+    pixel_values = torch.randn(batch_size, 3, 336, 336, dtype=torch.half, device=device)
+
+    image_features = model.image_embed(pixel_values, model_params)
+    image_features = model.image_embed(pixel_values, model_params)
+    image_features = model.image_embed(pixel_values, model_params)
+
+    start = time.perf_counter()
+    num_repeats = 10
+    for i in range(num_repeats):
+        image_features = model.image_embed(pixel_values, model_params)
+        print(image_features.shape)
+        torch.cuda.synchronize()
+    end = time.perf_counter()
+    duration = end - start
+    throughput = 576 * batch_size *  num_repeats / duration
+    return throughput
+
+def plot_embed_batchsize_throughput():
+    batch_sizes = [i for i in range(1, 8)]
+    throughputs = []
+    for batch_size in batch_sizes:
+        throughput = embed_batchsize_throughput(batch_size)
+        print(f"batch_size {batch_size} throughput {throughput} tokens/s")
+        throughputs.append(throughput)
+
+    bar_chart(batch_sizes, throughputs, figsize=(8, 6), xlabel="Batch Size", ylabel="Tokens per Second", title="Embed", filename="embed_bs2throughput")
+
 # 3. test embed
 if __name__ == '__main__':
-    plot_prefill_batchsize_throughput()
+    # plot_prefill_batchsize_throughput()
+    # plot_decode_batchsize_throughput()
+    plot_embed_batchsize_throughput()
