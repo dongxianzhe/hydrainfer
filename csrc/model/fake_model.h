120aace7f678fbf264f7774a4d8c7b06354d456a
Xianzhe Dong <dongxianzhe2019@gmail.com>
2024-12-16 21:29:10 -0800

    [feat] add fake model config used to test engine.

--- csrc/model/fake_model.h
+++ csrc/model/fake_model.h
@@ -3,13 +3,25 @@
 #include "linear.h"
 
 namespace mllm{
+struct FakeConfig{
+    int hidden_size = 4096;
+    int intermediate_size = 11008;
+    int n_qo_heads = 32;
+    int n_kv_heads = 32;
+    int head_size = 128;
+    int max_position_embeddings = 4096;
+    float rope_theta = 10000;
+    int n_layers = 32;
+    float rms_norm_eps = 1e-5;
+    int vocab_size = 32064;
+};
 
 class FakeModelImpl : public torch::nn::Module{
 public:
     int vocab_size_;
     torch::TensorOptions options_;
 public:
-    FakeModelImpl(int vocab_size, const torch::TensorOptions& options) : vocab_size_(vocab_size), options_(options){
+    FakeModelImpl(const FakeConfig& config, const torch::TensorOptions& options) : vocab_size_(config.vocab_size), options_(options){
     }
     torch::Tensor forward(torch::Tensor input_ids, torch::Tensor position_ids){
         int n_tokens = input_ids.size(0);
