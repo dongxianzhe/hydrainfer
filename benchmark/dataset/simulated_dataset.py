077fd9bb5f60146f56a2db8d137f2e6199ec5207
Xianzhe Dong <dongxianzhe2019@gmail.com>
2025-02-19 06:08:39 +0000

    [feat] support multi images, ttft slo, tpot slo for request.

--- benchmark/dataset/simulated_dataset.py
+++ benchmark/dataset/simulated_dataset.py
@@ -1,6 +1,6 @@
 from PIL import Image
 import base64
-from dataclasses import dataclass
+from dataclasses import dataclass, field, fields
 
 
 def encode_base64_content_from_path(image_path):
@@ -12,61 +12,49 @@ def encode_base64_content_from_path(image_path):
 @dataclass
 class SimulatedDataEntry:
     prompt: str
-    image: Image.Image 
-    image_base64: str
+    images: list[Image.Image] # PIL Image used in offline inference
+    image_base64: list[str] # str Image used in online inference
+    n_prompt_tokens: int # prompt token numbers
+    n_output_tokens: int # desired output token numbers
+    ttft_slo: float
+    tpot_slo: float
 
 
 class SimulatedDataset:
     """
-        an artifical dataset constructed used to profile engine in different scenarios
+        an synthetic dataset constructed used to profile engine in different scenarios
         all prompts' image share one image
         and prompts are same except length
-        if has image is true, SimulatedDataEntry will has an image and it's base64 encoding
-        prompt_text_len is text token len, not including image
     """
-    def __init__(self, tokenizer, image_path: str, has_images: list[bool], prompt_text_lens: list[int]):
-        for prompt_text_len in prompt_text_lens:
-            assert prompt_text_len > 0
-
+    def __init__(self, tokenizer, image_path: str, template_prompt: str="What is the content of this image?"):
         self.tokenizer = tokenizer
-
-        image = Image.open(image_path)
-        image_base64 = encode_base64_content_from_path(image_path=image_path)
+        self.template_prompt_tokens = self.tokenizer.encode(template_prompt)[1:] # skip <bos>
+        self.image_token = tokenizer.encode('<image>')[1:] # skip <bos>
+        self.image = Image.open(image_path)
+        self.image_base64 = encode_base64_content_from_path(image_path=image_path)
         self.dataset: list[SimulatedDataEntry] = []
 
-        max_tokens = max(prompt_text_lens)
-        question = self.tokenizer.encode( "What is the content of this image?")[1:] # skip <bos>
-        question = question * ((max_tokens  + len(question) - 1) // len(question))
-
-        for has_image, prompt_text_len in zip(has_images, prompt_text_lens):
-            if has_image:
-                prompt = "<image>" + tokenizer.decode(question[:prompt_text_len])
-            else:
-                prompt = tokenizer.decode(question[:prompt_text_len])
-            
-            self.dataset.append(SimulatedDataEntry(
-                prompt = prompt, 
-                image = image if has_image else None, 
-                image_base64 = image_base64 if has_image else None, 
-            ))
+    def append(self, n_prompt_tokens: int, n_images: int, n_output_tokens: int, ttft_slo: float, tpot_slo: float):
+        assert n_prompt_tokens > 0, 'n prompt tokens should greater than 0'
+        assert n_images >= 0, 'image number should greater than or equal to 0'
+        assert n_output_tokens > 0, 'n output tokens should greater than 0'
+        assert n_prompt_tokens >= n_images, 'prompt tokens number should at least greater than image numbers'
+        prompt_tokens = self.template_prompt_tokens * ((n_prompt_tokens  + len(self.template_prompt_tokens) - 1) // len(self.template_prompt_tokens))
+        prompt_tokens = self.image_token * n_images + prompt_tokens
+        prompt_tokens = prompt_tokens[:n_prompt_tokens]
+        prompt = self.tokenizer.decode(prompt_tokens)
+        self.dataset.append(SimulatedDataEntry(
+            prompt = prompt, 
+            images = [self.image] * n_images, 
+            image_base64 = [self.image_base64] * n_images, 
+            n_prompt_tokens = n_prompt_tokens, 
+            n_output_tokens = n_output_tokens, 
+            ttft_slo = ttft_slo, 
+            tpot_slo = tpot_slo
+        ))
     
     def __getitem__(self, index: int) -> SimulatedDataEntry:
         return self.dataset[index]
 
     def __len__(self):
         return len(self.dataset)
-
-
-if __name__ == '__main__':
-    from transformers import AutoProcessor
-    processor = AutoProcessor.from_pretrained('llava-hf/llava-1.5-7b-hf')
-    tokenizer = processor.tokenizer
-    dataset = SimulatedDataset(
-        tokenizer=tokenizer, 
-        image_path='./cherry_blossom.jpg',
-        has_images= [i < 12 for i in range(0, 25)], 
-        prompt_text_lens= list(range(1, 25)),
-        )
-
-    for i, entry in enumerate(dataset):
-        print(entry.prompt)
