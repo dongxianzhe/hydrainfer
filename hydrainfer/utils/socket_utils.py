099fbc77b7f8626ccf820b280d79e347ac9fa172
Xianzhe Dong <dongxianzhe2019@gmail.com>
2025-08-11 12:50:15 +0000

    feat: support auto config zmq and nccl ip address.

--- hydrainfer/utils/socket_utils.py
+++ hydrainfer/utils/socket_utils.py
@@ -1,6 +1,9 @@
 import socket
 from dataclasses import dataclass
-
+from typing import Optional
+from hydrainfer.utils.logger import getLogger
+import copy
+logger = getLogger(__name__)
 
 @dataclass
 class NetworkAddressConfig:
@@ -8,7 +11,7 @@ class NetworkAddressConfig:
     port: int = -1
 
 
-def find_free_port():
+def find_free_port() -> int:
     "return a port that is not used"
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind(('0.0.0.0', 0))
@@ -16,13 +19,36 @@ def find_free_port():
 
 
 def parse_port(port_config: int) -> int:
+    assert -1 <= port_config <= 65535, f'invalid port {port_config}'
     if port_config == -1:
         return find_free_port()
     return port_config
 
 
-def parse_address(config: NetworkAddressConfig):
-    port = parse_port(config.port)
-    assert 0 <= port <= 65535, f'invalid port {port}'
-    url = f"tcp://{config.host}:{port}"
+def parse_host(host_config: str) -> str:
+    if host_config == 'auto':
+        hostname = socket.gethostname()
+        ip_address = socket.gethostbyname(hostname)
+        return ip_address
+    else:
+        return host_config
+
+
+def parse_network_config(config: NetworkAddressConfig, log_name: Optional[str] = None) -> NetworkAddressConfig:
+    config = copy.deepcopy(config)
+    if config.host == 'auto':
+        config.host = parse_host(config.host)
+        if log_name:
+            logger.info(f'auto set {log_name} host to {config.host}')
+    if config.port == -1:
+        assert -1 <= config.port <= 65535, f'invalid port {config.port}'
+        config.port = parse_port(config.port)
+        if log_name:
+            logger.info(f'auto set {log_name} port to {config.port}')
+    return config
+
+
+def parse_address(config: NetworkAddressConfig) -> str:
+    config = parse_network_config(config)
+    url = f"tcp://{config.host}:{config.port}"
     return url
