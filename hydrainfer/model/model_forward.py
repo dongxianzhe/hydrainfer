07b0201dd343e98df12f406b0273630b06fb7db5
Xianzhe Dong <dongxianzhe2019@gmail.com>
2025-09-16 07:05:53 +0000

    feat: add general up down mlp layer.

--- hydrainfer/model/model_forward.py
+++ hydrainfer/model/model_forward.py
@@ -1,5 +1,22 @@
 from torch import nn, Tensor
 
+
+class UpDownMLP:
+    def __init__(
+        self, 
+        up_proj: nn.Linear, 
+        down_proj: nn.Linear, 
+        activation: nn.Module
+    ):
+        self.up_proj = up_proj
+        self.down_proj = down_proj
+        self.activation = activation
+
+    def forward(self, h: Tensor) -> Tensor:
+        # h (n_tokens, hidden_size)
+        return self.down_proj(self.activation(self.up_proj(h)))
+
+
 class GateUpDownMLP:
     def __init__(
         self, 
