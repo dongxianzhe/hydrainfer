0b2f24901346668ebde8df3af4b00bc45d29ae46
Xianzhe Dong <89450253+dongxianzhe@users.noreply.github.com>
2025-09-16 14:23:27 +0800

    feat: add fault taulerance when api server starting fails in evaluation script. (#17)
    Co-authored-by: Xianzhe Dong <dongxianzhe2019@gmail.com>

--- evaluation/test_correctness/evaluation.sh
+++ evaluation/test_correctness/evaluation.sh
@@ -10,35 +10,38 @@ port="8891"
 export CUDA_VISIBLE_DEVICES=1
 declare -A MODELS=(
     ["llava-hf/llava-1.5-7b-hf"]="/models/llava-1.5-7b-hf"
-    # ["llava-hf/llava-v1.6-vicuna-7b-hf"]="/models/llava-v1.6-vicuna-7b-hf"
-    # ["Qwen/Qwen2-VL-7B"]="/models/Qwen2-VL-7B/models--Qwen--Qwen2-VL-7B/snapshots/e61834264a23db10c06dc4f566dac5634c7ca024"
-    # ["deepseek-ai/deepseek-vl2-tiny"]="/models/deepseek-vl2-tiny"
+    ["llava-hf/llava-v1.6-vicuna-7b-hf"]="/models/llava-v1.6-vicuna-7b-hf"
+    ["Qwen/Qwen2-VL-7B"]="/models/Qwen2-VL-7B/models--Qwen--Qwen2-VL-7B/snapshots/e61834264a23db10c06dc4f566dac5634c7ca024"
+    ["deepseek-ai/deepseek-vl2-tiny"]="/models/deepseek-vl2-tiny"
 )
 
-for MODEL in "${!MODELS[@]}"; do
-    MODEL_PATH="${MODELS[$MODEL]}"
-    
-    echo "=============================================="
-    echo "Evaluating model: $MODEL"
-    echo "Path: $MODEL_PATH"
-    echo "=============================================="
-    
+start_server(){
     echo "starting api server"
-    RAY_DEDUP_LOGS=0 \
-        conda run -n hydrainfer --no-capture-output \
-        python -m hydrainfer.entrypoint.entrypoint \
-        model.path=$MODEL_PATH \
-        cluster.epdnode.executor.use_flash_infer=false \
-        cluster.epdnode.executor.multi_streams_forward=false \
-        cluster.epdnode.executor.multi_threads_forward=false \
-        cluster=single \
-        apiserver.host=$host \
-        apiserver.port=$port \
-        ignore_eos=true \
-        > $RESULT_PATH/${MODEL//\//-}-api_server.log 2>&1 &
+    max_retry=5
+    attempt=0
+    while [ $attempt -lt $max_retry ]; do
+        SCHEDULE_METHOD="${method}" \
+        RAY_DEDUP_LOGS=0 \
+            conda run -n hydrainfer --no-capture-output \
+            python -m hydrainfer.entrypoint.entrypoint \
+            model.path=$MODEL_PATH \
+            apiserver.host=$host \
+            apiserver.port=$port \
+            ignore_eos=true \
+            > $RESULT_PATH/${MODEL//\//-}-${method}-api_server.log 2>&1 &
+        pid=$!
+        if wait_api_server $host $port $pid; then
+            echo "server start success."
+            break
+        else
+            echo "server start failed. Retrying..."
+            clean_up
+            attempt=$((attempt + 1))
+        fi
+    done
+}
 
-    wait_api_server $host $port
-
+send_requests(){
     echo "start to send requests"
     conda run -n hydrainfer --no-capture-output \
         python ${OUR_ROOT_PATH}/benchmark/benchmark.py \
@@ -51,6 +54,18 @@ for MODEL in "${!MODELS[@]}"; do
         --request-rate $REQUEST_RATES \
         --backend=ours \
         --textcaps=1
+}
+
+for MODEL in "${!MODELS[@]}"; do
+    MODEL_PATH="${MODELS[$MODEL]}"
+    
+    echo "=============================================="
+    echo "Evaluating model: $MODEL"
+    echo "Path: $MODEL_PATH"
+    echo "=============================================="
+    
+    start_server
+    send_requests
 
     clean_up
     sleep 10
