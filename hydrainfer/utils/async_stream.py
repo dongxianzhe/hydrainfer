05c5fbf9459be42cec838d65030c0c532f5659c9
Xianzhe Dong <dongxianzhe2019@gmail.com>
2025-09-29 10:24:18 +0000

    feat: support token buffer in async stream.

--- hydrainfer/utils/async_stream.py
+++ hydrainfer/utils/async_stream.py
@@ -2,9 +2,10 @@ import asyncio
 from typing import Union
 
 class AsyncStream:
-    def __init__(self) -> None:
+    def __init__(self, interval: float=0) -> None:
         self._queue: asyncio.Queue = asyncio.Queue()
         self._finished = False
+        self.interval = interval
 
     def put(self, item: Union[str, Exception]) -> None:
         if self._finished:
@@ -26,4 +27,6 @@ class AsyncStream:
         result = await self._queue.get()
         if isinstance(result, Exception):
             raise result
+        if self.interval > 0:
+            await asyncio.sleep(self.interval)
         return result
