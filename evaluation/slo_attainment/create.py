c3f850a8b83f5e0b88431676c4fa03d1f97f9cd0
Xianzhe Dong <dongxianzhe2019@gmail.com>
2025-10-09 05:39:40 +0000

    feat: add sglang env setup.

--- evaluation/slo_attainment/create.py
+++ evaluation/slo_attainment/create.py
@@ -14,7 +14,7 @@ def create_conda_env(env_name, vllm_version):
             subprocess.run(f"conda create --name {env_name} python={args.python} -y", shell=True, check=True)
         else:
             print(f'conda env {env_name} already exists.')
-        subprocess.run(f"conda run -n {env_name} --no-capture-output pip install {args.package}=={vllm_version}", shell=True, check=True)
+        subprocess.run(f"conda run -n {env_name} --no-capture-output pip install {args.package}{args.extras}=={vllm_version}", shell=True, check=True)
     except subprocess.CalledProcessError as e:
         print(e)
         print(f"creating env {env_name} failed.")
@@ -30,6 +30,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Install specified package versions and create Conda environments")
     parser.add_argument('--python', type=str, required=True, help="Specify the Python version")
     parser.add_argument('--package', type=str, required=True, help="Specify the package name")
+    parser.add_argument('--extras', type=str, default="", help="package extra requirements")
     parser.add_argument('--versions', type=str, nargs='+', required=True, help="Specify multiple versions of the package")
 
     args = parser.parse_args()
--- evaluation/slo_attainment/setup_baseline_env.sh
+++ evaluation/slo_attainment/setup_baseline_env.sh
@@ -2,3 +2,4 @@
 set -e
 
 conda run -n base --no-capture-output python create.py --python=3.12 --package=vllm --versions 0.11.0 0.10.2 0.9.2 0.8.5 0.7.3 0.6.6
+conda run -n base --no-capture-output python create.py --python=3.12 --package=sglang --extras='[all]' --versions 0.5.3 0.4.10 0.4.3 0.3.6 0.2.15
